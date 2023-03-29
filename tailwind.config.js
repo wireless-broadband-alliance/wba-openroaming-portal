1be88b6d44a8a18139392de642fd642236f35308
Dário Santos <dariosantos@tetrapi.pt>
2023-03-29 10:29:57 +0000

    Added colors to config & bg image

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [
-    "./assets/**/*.js",
-    "./templates/**/*.html.twig",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: [
+		"./assets/**/*.js",
+		"./templates/**/*.html.twig"
+	],
+	theme: {
+		backgroundImage: {
+			landing: "url('/public/resources/images/wallpaper.png')"
+		},
+
+		colors: {
+			white: '#F5F5F5',
+
+			primary: '#A0C66B',
+		},
+
+		fontFamily: {
+			sans: ["Inter, sans-serif"],
+		},
+
+		extend: {},
+	},
+	plugins: [],
 }
