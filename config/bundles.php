b5cbcbd6daa64070d3af93170d62f4bc4eaca962
Nuno Facha <nunofacha@gmail.com>
2023-04-06 16:11:08 +0000

    Enable fixtures on all envs

--- config/bundles.php
+++ config/bundles.php
@@ -14,5 +14,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
 ];
