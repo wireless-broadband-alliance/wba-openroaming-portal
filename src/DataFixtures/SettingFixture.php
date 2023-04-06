c34b32dbd726ab5a76d69d863e858789cb596a39
Nuno Facha <nunofacha@gmail.com>
2023-04-06 18:30:55 +0000

    Rename fields

--- src/DataFixtures/SettingFixture.php
+++ src/DataFixtures/SettingFixture.php
@@ -12,9 +12,9 @@ class SettingFixture extends Fixture
     {
         $settings = [
             ['name' => 'RADIUS_REALM_NAME', 'value' => 'EditMe'],
-            ['name' => 'IOS_DISPLAY_NAME', 'value' => 'EditMe'],
-            ['name' => 'IOS_PAYLOAD_IDENTIFIER', 'value' => '887FAE2A-F051-4CC9-99BB-8DFD66F553A9'],
-            ['name' => 'IOS_OPERATOR_NAME', 'value' => 'EditMe'],
+            ['name' => 'DISPLAY_NAME', 'value' => 'EditMe'],
+            ['name' => 'PAYLOAD_IDENTIFIER', 'value' => '887FAE2A-F051-4CC9-99BB-8DFD66F553A9'],
+            ['name' => 'OPERATOR_NAME', 'value' => 'EditMe'],
             ['name' => 'DOMAIN_NAME', 'value' => 'EditMe'],
             ['name' => 'RADIUS_TLS_NAME', 'value' => 'EditMe'],
             ['name' => 'NAI_REALM', 'value' => 'EditMe'],
