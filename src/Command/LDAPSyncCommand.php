6b142fa8b1dc459ea17c056f7ba305b6e4c11f56
Nuno Facha <nunofacha@gmail.com>
2023-05-22 11:57:22 +0000

    Add LDAP sync support and check for expired passwords and locked accounts

--- src/Command/LDAPSyncCommand.php
+++ src/Command/LDAPSyncCommand.php
@@ -36,7 +36,22 @@ class LDAPSyncCommand extends Command
         $io->writeln('Found ' . count($ldapEnabledUsers) . ' LDAP enabled users');
         foreach ($ldapEnabledUsers as $user) {
             $io->writeln('Syncing ' . $user->saml_identifier . ' with LDAP');
-            $ldapUser = $this->fetchUserFromLDAP($user->saml_identifier);
+            $ldapUser = $this->fetchUserFromLDAP('or-demo');
+            if(!$ldapUser) {
+                $io->writeln('User ' . $user->saml_identifier . ' not found in LDAP, disabling');
+                continue;
+            }
+            $userAccountControl = $ldapUser['useraccountcontrol'][0];
+            $passwordExpired = ($userAccountControl & 0x800000) == 0x800000;
+            $userLocked = ($userAccountControl & 0x000002) == 0x000002;
+
+            if($userLocked){
+                $io->writeln('User ' . $user->saml_identifier . ' is locked in LDAP, disabling');
+            }else if($passwordExpired) {
+                $io->writeln('User ' . $user->saml_identifier . ' has an expired password in LDAP, disabling');
+            }else{
+                $io->writeln('User ' . $user->saml_identifier . ' is enabled in LDAP, enabling');
+            }
 
         }
 
@@ -45,6 +60,28 @@ class LDAPSyncCommand extends Command
 
     private function fetchUserFromLDAP(string $identifier)
     {
+        $ldapServer = 'ldap://172.17.0.53';
+        $ldapUsername = 'CN=ORSYNC,OU=System,OU=Admin,OU=Funcionarios,DC=CCASTANHEIRO,DC=PT';
+        $ldapPassword = '';
+        $ldapConnection = ldap_connect($ldapServer) or die("Could not connect to LDAP server.");
+        ldap_set_option($ldapConnection, LDAP_OPT_DEREF, LDAP_DEREF_ALWAYS);
+        ldap_set_option($ldapConnection, LDAP_OPT_PROTOCOL_VERSION, 3);
+        ldap_set_option($ldapConnection, LDAP_OPT_REFERRALS, 1);
+        ldap_bind($ldapConnection, $ldapUsername, $ldapPassword) or die("Could not bind to LDAP server.");
+        $searchFilter = "(sAMAccountName=$identifier)";
+        $searchBaseDN = "DC=CCASTANHEIRO,DC=PT";
+        $searchResult = ldap_search($ldapConnection, $searchBaseDN, $searchFilter);
 
+        ldap_get_option($ldapConnection, LDAP_OPT_REFERRALS, $referrals);
+        ldap_set_option($ldapConnection, LDAP_OPT_REFERRALS, $referrals);
+
+        $searchEntries = ldap_get_entries($ldapConnection, $searchResult);
+        ldap_unbind($ldapConnection);
+
+        if ($searchEntries['count'] === 1) {
+            return $searchEntries[0];
+        } else {
+            return null;
+        }
     }
 }
