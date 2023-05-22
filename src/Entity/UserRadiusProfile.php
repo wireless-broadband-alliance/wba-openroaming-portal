2930f80443d191148cb00ef7fb7be2b2a66e7968
Nuno Facha <nunofacha@gmail.com>
2023-05-22 15:47:13 +0000

    Add issued_at, valid_until, and status fields to UserRadiusProfile entity

--- src/Entity/UserRadiusProfile.php
+++ src/Entity/UserRadiusProfile.php
@@ -23,6 +23,15 @@ class UserRadiusProfile
     #[ORM\Column(length: 255)]
     private ?string $radius_user = null;
 
+    #[ORM\Column]
+    private ?\DateTimeImmutable $issued_at = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $valid_until = null;
+
+    #[ORM\Column]
+    private ?int $status = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -60,6 +69,42 @@ class UserRadiusProfile
     public function setRadiusUser(string $radius_user): self
     {
         $this->radius_user = $radius_user;
+
+        return $this;
+    }
+
+    public function getIssuedAt(): ?\DateTimeImmutable
+    {
+        return $this->issued_at;
+    }
+
+    public function setIssuedAt(\DateTimeImmutable $issued_at): self
+    {
+        $this->issued_at = $issued_at;
+
+        return $this;
+    }
+
+    public function getValidUntil(): ?\DateTimeImmutable
+    {
+        return $this->valid_until;
+    }
+
+    public function setValidUntil(?\DateTimeImmutable $valid_until): self
+    {
+        $this->valid_until = $valid_until;
+
+        return $this;
+    }
+
+    public function getStatus(): ?int
+    {
+        return $this->status;
+    }
+
+    public function setStatus(int $status): self
+    {
+        $this->status = $status;
 
         return $this;
     }
