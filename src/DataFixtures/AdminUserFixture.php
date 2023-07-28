a2d37420294b8cf4f346fd50d8be8f53db09bd2a
Nuno Facha <nunofacha@gmail.com>
2023-07-28 11:35:32 +0000

    Update admin user's email and password, remove bot user and pagination tests

--- src/DataFixtures/AdminUserFixture.php
+++ src/DataFixtures/AdminUserFixture.php
@@ -5,8 +5,8 @@ namespace App\DataFixtures;
 
 use App\Entity\User;
 use DateTime;
-use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AdminUserFixture extends Fixture
@@ -20,36 +20,12 @@ class AdminUserFixture extends Fixture
     {
         $admin = new User();
         $admin->setUuid('admin');
-        $admin->setEmail('admin@wba.com');
-        $admin->setPassword($this->userPasswordHashed->hashPassword($admin, 'pancakes'));
+        $admin->setEmail('admin@example.com');
+        $admin->setPassword($this->userPasswordHashed->hashPassword($admin, 'gnimaornepo'));
         $admin->setRoles(['ROLE_ADMIN']);
         $admin->setIsVerified(true);
         $admin->setCreatedAt(new DateTime());
         $manager->persist($admin);
-
-        $user1 = new User();
-        $user1->setUuid('bot');
-        $user1->setEmail('bot@wba.com');
-        $user1->setPassword($this->userPasswordHashed->hashPassword($admin, '123'));
-        $user1->setIsVerified(true);
-        $user1->setCreatedAt(new DateTime());
-        $manager->persist($user1);
-
-
-        // Create 10 additional users, this is only for testing the pagination system
-        /*
-        for ($i = 1; $i <= 100; $i++) {
-            $user = new User();
-            $user->setUuid('user'.$i);
-            $user->setEmail('user'.$i.'@example.com');
-            $user->setPassword($this->userPasswordHashed->hashPassword($user, '1234567'));
-            $user->setRoles(['ROLE_USER']);
-            $user->setIsVerified(true);
-            $user->setCreatedAt(new DateTime());
-
-            $manager->persist($user);
-        }
-        */
 
         $manager->flush();
     }
