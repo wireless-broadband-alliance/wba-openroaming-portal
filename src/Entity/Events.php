75c451da374b87646ab0463efd6f77eeb297569b
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2023-08-31 10:06:54 +0000

    Events table

---
+++ src/Entity/Events.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EventsRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EventsRepository::class)]
+class Events
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $event_datetime = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $event_name = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $event_metadata = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEventDatetime(): ?\DateTimeInterface
+    {
+        return $this->event_datetime;
+    }
+
+    public function setEventDatetime(\DateTimeInterface $event_datetime): static
+    {
+        $this->event_datetime = $event_datetime;
+
+        return $this;
+    }
+
+    public function getEventName(): ?string
+    {
+        return $this->event_name;
+    }
+
+    public function setEventName(string $event_name): static
+    {
+        $this->event_name = $event_name;
+
+        return $this;
+    }
+
+    public function getEventMetadata(): ?string
+    {
+        return $this->event_metadata;
+    }
+
+    public function setEventMetadata(string $event_metadata): static
+    {
+        $this->event_metadata = $event_metadata;
+
+        return $this;
+    }
+}
---
+++ src/Repository/EventsRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Events;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Events>
+ *
+ * @method Events|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Events|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Events[]    findAll()
+ * @method Events[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class EventsRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Events::class);
+    }
+
+//    /**
+//     * @return Events[] Returns an array of Events objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('e')
+//            ->andWhere('e.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('e.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Events
+//    {
+//        return $this->createQueryBuilder('e')
+//            ->andWhere('e.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
