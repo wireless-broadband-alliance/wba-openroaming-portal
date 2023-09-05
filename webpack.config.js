e2f67f1b7e01c4a635d36ea5b45ee59cc6072e1b
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2023-09-05 12:23:36 +0000

    Forgot to remove this comment, and the script tag

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ Encore
 	 * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
 	 */
 	.addEntry('app', './assets/app.js')
-	.addEntry('chartjs', 'chart.js') // Need it for chart.js
+	.addEntry('chartjs', 'chart.js')
 
 	// enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
 	.enableStimulusBridge('./assets/controllers.json')
