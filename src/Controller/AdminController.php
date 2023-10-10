acee820b3c434d0878cacff51c25a16285a4799a
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2023-10-10 15:27:48 +0000

    Start new form for terms and policies

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -638,6 +638,35 @@ class AdminController extends AbstractController
         ]);
     }
 
+    /**
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @param GetSettings $getSettings
+     * @return Response
+     */
+    #[Route('/dashboard/settings/terms', name: 'admin_dashboard_settings_terms')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function settings_terms(Request $request, EntityManagerInterface $em, GetSettings $getSettings): Response
+    {
+        // Get the current logged-in user (admin)
+        /** @var User $currentUser */
+        $currentUser = $this->getUser();
+        if (!$currentUser->IsVerified()) {
+            $this->addFlash('error_admin', 'Your account is not verified. Please check your email.');
+            return $this->redirectToRoute('admin_confirm_reset', ['type' => 'password']);
+        }
+
+        // Call the getSettings method of GetSettings class to retrieve the data
+        $data = $this->getSettings->getSettings($this->userRepository, $this->settingRepository);
+
+
+        return $this->render('admin/terms.html.twig', [
+            'data' => $data,
+            'getSettings' => $getSettings,
+            'current_user' => $currentUser
+        ]);
+    }
+
     /*This route it's in development, again I need to fix and check for another stuff first
     #[Route('/dashboard/statistics', name: 'admin_dashboard_statistics')]
     #[IsGranted('ROLE_ADMIN')]
