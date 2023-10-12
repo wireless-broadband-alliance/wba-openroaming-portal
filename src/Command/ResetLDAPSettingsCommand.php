3ce70a1cad6528675289d8919c51ae59696b9ea8
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2023-10-12 15:57:14 +0000

    Reset LDAP and All the settings on the system

--- src/Command/ResetLDAPSettingsCommand.php
+++ src/Command/ResetLDAPSettingsCommand.php
@@ -14,10 +14,10 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 #[AsCommand(
-    name: 'reset:allSettings',
-    description: 'Reset All Settings',
+    name: 'reset:ldapSettings',
+    description: 'Reset LDAP Settings',
 )]
-class ResetMainSettingsCommand extends Command
+class ResetLDAPSettingsCommand extends Command
 {
     private EntityManagerInterface $entityManager;
 
@@ -31,8 +31,8 @@ class ResetMainSettingsCommand extends Command
     protected function configure(): void
     {
         $this
-            ->setName('reset:allSettings')
-            ->setDescription('Reset All Settings')
+            ->setName('reset:ldapSettings')
+            ->setDescription('Reset LDAP Settings')
             ->addOption('yes', 'y', InputOption::VALUE_NONE, 'Automatically confirm the reset');
     }
 
@@ -41,7 +41,7 @@ class ResetMainSettingsCommand extends Command
         // Check if the --yes option is provided (comes from a controller), then skip the confirmation prompt
         if (!$input->getOption('yes')) {
             $helper = $this->getHelper('question');
-            $question = new ConfirmationQuestion('This action will reset the main settings. [y/N] ', false);
+            $question = new ConfirmationQuestion('This action will reset the LDAP settings. [y/N] ', false);
             /** @var QuestionHelper $helper */
             if (!$helper->ask($input, $output, $question)) {
                 $output->writeln('Command aborted.');
@@ -50,51 +50,12 @@ class ResetMainSettingsCommand extends Command
         }
 
         $settings = [
-            ['name' => 'RADIUS_REALM_NAME', 'value' => 'EditMe'],
-            ['name' => 'DISPLAY_NAME', 'value' => 'EditMe'],
-            ['name' => 'PAYLOAD_IDENTIFIER', 'value' => '887FAE2A-F051-4CC9-99BB-8DFD66F553A9'],
-            ['name' => 'OPERATOR_NAME', 'value' => 'EditMe'],
-            ['name' => 'DOMAIN_NAME', 'value' => 'EditMe'],
-            ['name' => 'RADIUS_TLS_NAME', 'value' => 'EditMe'],
-            ['name' => 'NAI_REALM', 'value' => 'EditMe'],
-            ['name' => 'RADIUS_TRUSTED_ROOT_CA_SHA1_HASH', 'value' => 'ca bd 2a 79 a1 07 6a 31 f2 1d 25 36 35 cb 03 9d 43 29 a5 e8'],
-
-            ['name' => 'PLATFORM_MODE', 'value' => 'Demo'],
-            ['name' => 'EMAIL_VERIFICATION', 'value' => 'OFF'],
-
-            ['name' => 'PAGE_TITLE', 'value' => 'OpenRoaming Portal'],
-            ['name' => 'CUSTOMER_LOGO', 'value' => '/resources/logos/WBA_20th_logo.png'],
-            ['name' => 'OPENROAMING_LOGO', 'value' => '/resources/logos/openroaming.svg'],
-            ['name' => 'WALLPAPER_IMAGE', 'value' => '/resources/images/wallpaper.png'],
-            ['name' => 'WELCOME_TEXT', 'value' => 'Welcome to OpenRoaming Provisioning Service'],
-            ['name' => 'WELCOME_DESCRIPTION', 'value' => 'This provisioning portal is for the WBA OpenRoaming Live Program'],
-            ['name' => 'ADDITIONAL_LABEL', 'value' => 'This label it\'s to add extra content if necessary'],
-            ['name' => 'CONTACT_EMAIL', 'value' => 'duck-ops@example.com'],
-
-            ['name' => 'AUTH_METHOD_SAML_ENABLED', 'value' => 'false'],
-            ['name' => 'AUTH_METHOD_SAML_LABEL', 'value' => 'Login with SAML'],
-            ['name' => 'AUTH_METHOD_SAML_DESCRIPTION', 'value' => 'Authenticate with your work account'],
-            ['name' => 'AUTH_METHOD_GOOGLE_LOGIN_ENABLED', 'value' => 'false'],
-            ['name' => 'AUTH_METHOD_GOOGLE_LOGIN_LABEL', 'value' => 'Login with Google'],
-            ['name' => 'AUTH_METHOD_GOOGLE_LOGIN_DESCRIPTION', 'value' => 'Authenticate with your Google account'],
-            ['name' => 'AUTH_METHOD_REGISTER_ENABLED', 'value' => 'true'],
-            ['name' => 'AUTH_METHOD_REGISTER_LABEL', 'value' => 'Create Account'],
-            ['name' => 'AUTH_METHOD_REGISTER_DESCRIPTION', 'value' => 'Don\'t have an account? Create one'],
-            ['name' => 'AUTH_METHOD_LOGIN_TRADITIONAL_ENABLED', 'value' => 'true'],
-            ['name' => 'AUTH_METHOD_LOGIN_TRADITIONAL_LABEL', 'value' => 'Account Login'],
-            ['name' => 'AUTH_METHOD_LOGIN_TRADITIONAL_DESCRIPTION', 'value' => 'Already have an account? Login then'],
-
             ['name' => 'SYNC_LDAP_ENABLED', 'value' => 'false'],
             ['name' => 'SYNC_LDAP_SERVER', 'value' => 'ldap://127.0.0.1'],
             ['name' => 'SYNC_LDAP_BIND_USER_DN', 'value' => ''],
             ['name' => 'SYNC_LDAP_BIND_USER_PASSWORD', 'value' => ''],
             ['name' => 'SYNC_LDAP_SEARCH_BASE_DN', 'value' => ''],
             ['name' => 'SYNC_LDAP_SEARCH_FILTER', 'value' => '(sAMAccountName=$identifier)'],
-
-            ['name' => 'TOS_LINK', 'value' => 'https://wballiance.com/openroaming/toc/'],
-            ['name' => 'PRIVACY_POLICY_LINK', 'value' => 'https://wballiance.com/openroaming/privacy-policy'],
-            ['name' => 'VALID_DOMAINS_GOOGLE_LOGIN', 'value' => ''],
-            ['name' => 'PROFILES_ENCRYPTION_TYPE_IOS_ONLY', 'value' => 'WPA2'],
         ];
 
         // Begin a database transaction to ensure data consistency
@@ -127,7 +88,7 @@ class ResetMainSettingsCommand extends Command
 
             $message = <<<EOL
 
-<info>Success:</info> All the settings have been set to the default values.
+<info>Success:</info> All the LDAP settings have been set to the default values.
 <comment>Note:</comment> If you want to reset the custom settings too,
        make sure to run the following command:
        <fg=blue>php bin/console reset:customS</>
