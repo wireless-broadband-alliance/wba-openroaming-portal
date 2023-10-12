9b44afe2781756830d2608bd3fd9a88289f4a657
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2023-10-12 17:16:14 +0000

    Reset Capport Settings Command

---
+++ src/Command/ResetCapportSettingsCommand.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\Setting;
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\QuestionHelper;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+
+#[AsCommand(
+    name: 'reset:capportSettings',
+    description: 'Reset Capport configuration Settings',
+)]
+class ResetCapportSettingsCommand extends Command
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setName('reset:capportSettings')
+            ->setDescription('Reset Capport configuration Settings')
+            ->addOption('yes', 'y', InputOption::VALUE_NONE, 'Automatically confirm the reset');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        if (!$input->getOption('yes')) {
+            $helper = $this->getHelper('question');
+            $question = new ConfirmationQuestion('This action will reset the capport configuration settings. [y/N] ', false);
+            /** @var QuestionHelper $helper */
+            if (!$helper->ask($input, $output, $question)) {
+                $output->writeln('Command aborted.');
+                return Command::SUCCESS;
+            }
+        }
+
+        $settings = [
+            ['name' => 'CAPPORT_ENABLED', 'value' => 'false'],
+            ['name' => 'CAPPORT_PORTAL_URL', 'value' => 'https://example.com/'],
+            ['name' => 'CAPPORT_VENUE_INFO_URL', 'value' => 'https://example.com/'],
+        ];
+
+        $this->entityManager->beginTransaction();
+
+        try {
+            $settingsRepository = $this->entityManager->getRepository(Setting::class);
+
+            foreach ($settings as $settingData) {
+                $name = $settingData['name'];
+                $value = $settingData['value'];
+
+                $setting = $settingsRepository->findOneBy(['name' => $name]);
+
+                if ($setting) {
+                    // Update the already existing value
+                    $setting->setValue($value);
+                } else {
+                    $setting = new Setting();
+                    $setting->setName($name);
+                    $setting->setValue($value);
+                    $this->entityManager->persist($setting);
+                }
+            }
+
+            $this->entityManager->flush();
+            $this->entityManager->commit();
+
+            $message = <<<EOL
+
+<info>Success:</info> The Capport Configuration settings have been set to the default values.
+<comment>Note:</comment> If you want to reset any another setting please check using this command:
+      <fg=blue>php bin/console reset</>
+EOL;
+
+            $output->write($message);
+            $output->writeln(['']);
+        } catch (Exception $e) {
+            $this->entityManager->rollback();
+            $output->writeln('An error occurred while resetting settings: ' . $e->getMessage());
+            return Command::FAILURE;
+        }
+
+        return Command::SUCCESS;
+    }
+}
