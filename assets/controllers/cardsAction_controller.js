05615f33658f095a14e6e0eb10df9be6b76ee6f5
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2023-10-16 20:33:17 +0000

    Calling the function to block each type of input

--- assets/controllers/cardsAction_controller.js
+++ assets/controllers/cardsAction_controller.js
@@ -64,5 +64,75 @@ export default class extends Controller {
 			capportTextInputs,
 			capportCards
 		);
+
+
+		// SAML
+		const samlTextInputs = [
+			document.querySelector('[name="auth[AUTH_METHOD_SAML_LABEL]"]'),
+			document.querySelector('[name="auth[AUTH_METHOD_SAML_DESCRIPTION]"]')
+		];
+
+		const samlCards = [
+			document.getElementById('AUTH_METHOD_SAML_LABEL'),
+			document.getElementById('AUTH_METHOD_SAML_DESCRIPTION')
+		];
+
+		CardsActions(
+			document.querySelector('[name="auth[AUTH_METHOD_SAML_ENABLED]"]'),
+			samlTextInputs,
+			samlCards
+		);
+
+		// Google
+		const googleTextInputs = [
+			document.querySelector('[name="auth[AUTH_METHOD_GOOGLE_LOGIN_LABEL]"]'),
+			document.querySelector('[name="auth[AUTH_METHOD_GOOGLE_LOGIN_DESCRIPTION]"]')
+		];
+
+		const googleCards = [
+			document.getElementById('AUTH_METHOD_GOOGLE_LOGIN_LABEL'),
+			document.getElementById('AUTH_METHOD_GOOGLE_LOGIN_DESCRIPTION')
+		];
+
+		CardsActions(
+			document.querySelector('[name="auth[AUTH_METHOD_GOOGLE_LOGIN_ENABLED]"]'),
+			googleTextInputs,
+			googleCards
+		);
+
+		// REGISTER
+		const registerTextInputs = [
+			document.querySelector('[name="auth[AUTH_METHOD_REGISTER_LABEL]"]'),
+			document.querySelector('[name="auth[AUTH_METHOD_REGISTER_DESCRIPTION]"]')
+		];
+
+		const registerCards = [
+			document.getElementById('AUTH_METHOD_REGISTER_LABEL'),
+			document.getElementById('AUTH_METHOD_REGISTER_DESCRIPTION')
+		];
+
+		CardsActions(
+			document.querySelector('[name="auth[AUTH_METHOD_REGISTER_ENABLED]"]'),
+			registerTextInputs,
+			registerCards
+		);
+
+		// TRADITIONAL LOGIN
+		const traditionalLoginTextInputs = [
+			document.querySelector('[name="auth[AUTH_METHOD_LOGIN_TRADITIONAL_LABEL]"]'),
+			document.querySelector('[name="auth[AUTH_METHOD_LOGIN_TRADITIONAL_DESCRIPTION]"]')
+		];
+
+		const traditionalLoginCards = [
+			document.getElementById('AUTH_METHOD_LOGIN_TRADITIONAL_LABEL'),
+			document.getElementById('AUTH_METHOD_LOGIN_TRADITIONAL_DESCRIPTION')
+		];
+
+		CardsActions(
+			document.querySelector('[name="auth[AUTH_METHOD_LOGIN_TRADITIONAL_ENABLED]"]'),
+			traditionalLoginTextInputs,
+			traditionalLoginCards
+		);
+
 	}
 }
