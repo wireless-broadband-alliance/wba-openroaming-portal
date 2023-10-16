c7566b5edd1500fc790e37ddd67ca3eb97e812f4
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2023-10-16 16:59:15 +0000

    Making the CardsActions more reusable

--- assets/controllers/cardsAction_controller.js
+++ assets/controllers/cardsAction_controller.js
@@ -2,38 +2,24 @@ import {Controller} from '@hotwired/stimulus';
 
 export default class extends Controller {
 	connect() {
-		function DisableEnableCards(
+		function CardsActions(
 			selectInput,
-			firstInput,
-			secondInput,
-			firstCard,
-			secondCard,
+			textInputs,
+			cards
 		) {
 			if (selectInput) {
 				const toggleInputState = () => {
-					const capportEnabledValue = selectInput.value;
+					const isEnabled = selectInput.value === 'true';
 
-					if (capportEnabledValue === 'true') {
-						firstInput.disabled = false;
-						secondInput.disabled = false;
+					textInputs.forEach((input) => {
+						input.disabled = !isEnabled;
+						input.classList.toggle('cursor-not-allowed', !isEnabled);
+					});
 
-						firstInput.classList.remove('cursor-not-allowed');
-						secondInput.classList.remove('cursor-not-allowed');
-						firstCard.classList.add('bg-white');
-						secondCard.classList.add('bg-white');
-						firstCard.classList.remove('bg-disableCardsColor');
-						secondCard.classList.remove('bg-disableCardsColor');
-					} else {
-						firstInput.disabled = true;
-						secondInput.disabled = true;
-
-						firstInput.classList.add('cursor-not-allowed');
-						secondInput.classList.add('cursor-not-allowed');
-						firstCard.classList.remove('bg-white');
-						secondCard.classList.remove('bg-white');
-						firstCard.classList.add('bg-disableCardsColor');
-						secondCard.classList.add('bg-disableCardsColor');
-					}
+					cards.forEach((card) => {
+						card.classList.toggle('bg-white', isEnabled);
+						card.classList.toggle('bg-disableCardsColor', !isEnabled);
+					});
 				};
 
 				toggleInputState();
@@ -41,13 +27,42 @@ export default class extends Controller {
 			}
 		}
 
-		// Call the functions
-		DisableEnableCards(
-			document.querySelector('[name="capport[CAPPORT_ENABLED]"]'),
+		const ldapTextInputs = [];
+		const ldapCards = [];
+
+		// Iterate over LDAP settings to populate the arrays
+		const ldapSettings = ['SYNC_LDAP_SERVER', 'SYNC_LDAP_BIND_USER_DN', 'SYNC_LDAP_BIND_USER_PASSWORD', 'SYNC_LDAP_SEARCH_BASE_DN', 'SYNC_LDAP_SEARCH_FILTER'];
+
+		ldapSettings.forEach(settingName => {
+			const textInput = document.querySelector(`[name="ldap[${settingName}]"]`);
+			const card = document.getElementById(settingName);
+
+			if (textInput && card) {
+				ldapTextInputs.push(textInput);
+				ldapCards.push(card);
+			}
+		});
+
+		CardsActions(
+			document.querySelector('[name="ldap[SYNC_LDAP_ENABLED]"]'),
+			ldapTextInputs,
+			ldapCards
+		);
+
+
+		const capportTextInputs = [
 			document.querySelector('[name="capport[CAPPORT_PORTAL_URL]"]'),
-			document.querySelector('[name="capport[CAPPORT_VENUE_INFO_URL]"]'),
+			document.querySelector('[name="capport[CAPPORT_VENUE_INFO_URL]"]')
+		];
+
+		const capportCards = [
 			document.getElementById('CAPPORT_PORTAL_URL'),
-			document.getElementById('CAPPORT_VENUE_INFO_URL'),
+			document.getElementById('CAPPORT_VENUE_INFO_URL')
+		];
+		CardsActions(
+			document.querySelector('[name="capport[CAPPORT_ENABLED]"]'),
+			capportTextInputs,
+			capportCards
 		);
 	}
 }
