d543bd7f284096e49bde2ac29cb946e56f9651cb
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2023-10-16 09:53:09 +0000

    Correct color for submit buttons from Angela
    #232B35 and not #1C0523

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,7 @@ module.exports = {
 
 				primary: '#A0C66B',
 				veryDarkPurple: '#594B60',
-				veryDarkButton: '#1C0523',
+				veryDarkButton: '#232B35',
 				lightGreen: '#57a475',
 				veryLightGreen: '#f1f8f4',
 				lightGray: '#f7f6f8',
