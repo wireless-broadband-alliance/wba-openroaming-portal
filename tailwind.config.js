02939d95a37655e76b3cce26c3189e35ad964788
Tiago Feitor <tiagofeitor@tetrapi.pt>
2023-10-17 19:22:00 +0000

    Capport page

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,6 @@ module.exports = {
 	],
 	theme: {
 		extend: {
-
-
 			colors: {
 				gray: {
 					100: '#F5F5F5',
@@ -21,6 +19,9 @@ module.exports = {
 				veryLightGreen: '#f1f8f4',
 				lightGray: '#f7f6f8',
 				inputGray: '#D9D9D9',
+				lightBlue: "#DFEBFC",
+				textBlue: "#1D65A7"
+
 			},
 
 			fontFamily: {
