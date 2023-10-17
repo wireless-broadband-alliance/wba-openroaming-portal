324391ed96c085de863033c82f0196a63fe7bb7e
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2023-10-17 22:04:48 +0000

    DONE all the cards and stuff, related to selected icons

--- assets/controllers/showIconRadios_controller.js
+++ assets/controllers/showIconRadios_controller.js
@@ -27,7 +27,7 @@ export default class extends Controller {
 			});
 		}
 
-		// JavaScript to display the correct icon when the page loads
+		// Looks for the first element with the name declared
 		document.addEventListener("DOMContentLoaded", function () {
 			const radioSets = document.querySelectorAll('[name="Cards"]');
 
@@ -38,6 +38,45 @@ export default class extends Controller {
 				const offCustomRadio = radioSet.querySelector('[name="offCustomRadio"]');
 
 				initializeRadioButtons(onLabel, offLabel, onCustomRadio, offCustomRadio);
+			});
+		});
+
+		// Looks for the specific name second name element
+		document.addEventListener("DOMContentLoaded", function () {
+			const GoogleRadioSets = document.querySelectorAll('[name="GoogleCards"]');
+			GoogleRadioSets.forEach(function (radioSet) {
+				const onLabelGoogle = radioSet.querySelector('[name="onLabelGoogle"]');
+				const offLabelGoogle = radioSet.querySelector('[name="offLabelGoogle"]');
+				const onCustomRadioGoogle = radioSet.querySelector('[name="onCustomRadioGoogle"]');
+				const offCustomRadioGoogle = radioSet.querySelector('[name="offCustomRadioGoogle"]');
+
+				initializeRadioButtons(onLabelGoogle, offLabelGoogle, onCustomRadioGoogle, offCustomRadioGoogle);
+			});
+		});
+
+		// Looks for the specific name third name element
+		document.addEventListener("DOMContentLoaded", function () {
+			const RegisterRadioSets = document.querySelectorAll('[name="RegisterCards"]');
+			RegisterRadioSets.forEach(function (radioSet) {
+				const onLabelRegister = radioSet.querySelector('[name="onLabelRegister"]');
+				const offLabelRegister = radioSet.querySelector('[name="offLabelRegister"]');
+				const onCustomRadioRegister = radioSet.querySelector('[name="onCustomRadioRegister"]');
+				const offCustomRadioRegister = radioSet.querySelector('[name="offCustomRadioRegister"]');
+
+				initializeRadioButtons(onLabelRegister, offLabelRegister, onCustomRadioRegister, offCustomRadioRegister);
+			});
+		});
+
+		// Looks for the specific name fourth name element
+		document.addEventListener("DOMContentLoaded", function () {
+			const LoginRadioSets = document.querySelectorAll('[name="LoginCards"]');
+			LoginRadioSets.forEach(function (radioSet) {
+				const onLabelLogin = radioSet.querySelector('[name="onLabelLogin"]');
+				const offLabelLogin = radioSet.querySelector('[name="offLabelLogin"]');
+				const onCustomRadioLogin = radioSet.querySelector('[name="onCustomRadioLogin"]');
+				const offCustomRadioLogin = radioSet.querySelector('[name="offCustomRadioLogin"]');
+
+				initializeRadioButtons(onLabelLogin, offLabelLogin, onCustomRadioLogin, offCustomRadioLogin);
 			});
 		});
 	}
