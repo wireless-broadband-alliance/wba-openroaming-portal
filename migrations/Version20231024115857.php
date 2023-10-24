adfe1376303bf7d5f05936093a68aade7dda1838
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2023-10-24 12:15:28 +0000

    New Field deledtedAt for users, reworking delete users logic

---
+++ migrations/Version20231024115857.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231024115857 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE User ADD deletedAt DATETIME DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE User DROP deletedAt, CHANGE email email VARCHAR(255) DEFAULT NULL, CHANGE createdAt createdAt DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, CHANGE bannedAt bannedAt DATETIME DEFAULT CURRENT_TIMESTAMP');
+    }
+}
