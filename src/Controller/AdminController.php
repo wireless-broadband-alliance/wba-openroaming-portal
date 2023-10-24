3e071412665b6643008a38352de916476ab81295
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2023-10-24 09:56:03 +0000

    rename again

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -806,8 +806,8 @@ class AdminController extends AbstractController
                 'PROFILES_ENCRYPTION_TYPE_IOS_ONLY',
             ];
 
-            $static_value = '887FAE2A-F051-4CC9-99BB-8DFD66F553A9';
-            if ($submittedData['PAYLOAD_IDENTIFIER'] === $static_value) {
+            $staticValue = '887FAE2A-F051-4CC9-99BB-8DFD66F553A9';
+            if ($submittedData['PAYLOAD_IDENTIFIER'] === $staticValue) {
                 $this->addFlash('error_admin', 'Please do not use the default value from the Payload Identifier card.');
                 return $this->redirectToRoute('admin_dashboard_settings_radius');
             }
