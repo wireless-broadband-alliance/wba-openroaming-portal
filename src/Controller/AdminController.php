299af028c506af87e4514517b4611784f71b723f
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2023-10-25 11:03:55 +0000

    Show bar graphic with fake data
    Start a base design for the statistics page

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1148,43 +1148,48 @@ class AdminController extends AbstractController
         $data = $this->getSettings->getSettings($this->userRepository, $this->settingRepository);
         $user = $this->getUser();
 
-        $fetchChart = $this->fetchChartData();
-
         // Create a Chart object and set the data
-        $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
-        $chart->setData($fetchChart);
+        $fetchChartDevices = $this->fetchChartDevices();
+        $devices = $chartBuilder->createChart(Chart::TYPE_BAR); // This declares the type of graphic used
+        $devices->setData($fetchChartDevices);
 
         return $this->render('admin/statistics.html.twig', [
             'data' => $data,
             'current_user' => $user,
-            'chart' => $chart, // Pass the Chart object to the template
-            'chartDataJson' => json_encode($fetchChart, JSON_THROW_ON_ERROR),
+            'devices' => $devices, // Pass the Chart object to the template
+            'devicesDataJson' => json_encode($fetchChartDevices, JSON_THROW_ON_ERROR),
         ]);
     }
 
-
-    /**
-     * @return array|JsonResponse
-     */
-    private function fetchChartData(): JsonResponse|array
+    private function fetchChartDevices(): JsonResponse|array
     {
         // This is fake data just for testing
         return [
             'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
             'datasets' => [
                 [
-                    'label' => 'Cookies eaten 🍪',
+                    'label' => 'Mac',
                     'backgroundColor' => 'rgb(255, 99, 132, .4)',
                     'borderColor' => 'rgb(255, 99, 132)',
                     'data' => [2, 10, 5, 18, 20, 30, 45],
-                    'tension' => 0.4,
                 ],
                 [
-                    'label' => 'Km walked 🏃‍♀️',
+                    'label' => 'iOS️',
                     'backgroundColor' => 'rgba(45, 220, 126, .4)',
                     'borderColor' => 'rgba(45, 220, 126)',
                     'data' => [10, 15, 4, 3, 25, 41, 25],
-                    'tension' => 0.4,
+                ],
+                [
+                    'label' => 'Android',
+                    'backgroundColor' => 'rgba(45, 220, 126, .4)',
+                    'borderColor' => 'rgba(45, 220, 126)',
+                    'data' => [10, 15, 4, 3, 25, 41, 25],
+                ],
+                [
+                    'label' => 'Windows',
+                    'backgroundColor' => 'rgba(45, 220, 126, .4)',
+                    'borderColor' => 'rgba(45, 220, 126)',
+                    'data' => [10, 15, 4, 3, 25, 41, 25],
                 ],
             ],
         ];
