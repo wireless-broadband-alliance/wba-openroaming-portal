e21466785113688fba371c9ba1eb99fc2a237393
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2023-10-30 12:21:15 -0100

    Removing the testing fixtures, Improve date inputs design

--- src/DataFixtures/AdminUserFixture.php
+++ src/DataFixtures/AdminUserFixture.php
@@ -46,48 +46,6 @@ class AdminUserFixture extends Fixture
         $manager->persist($event_2);
 
 
-        for ($i = 0; $i < 10; $i++) {
-            $user = new User();
-            $user->setUuid('user' . $i . '@example.com');
-            $user->setEmail('user' . $i . '@example.com');
-            $user->setPassword($this->userPasswordHashed->hashPassword($user, 'password123')); // Set a default password
-            $user->setIsVerified(true);
-            $user->setCreatedAt(new DateTime());
-            $manager->persist($user);
-
-            $event = new Event();
-            $event->setEventDatetime(new DateTime());
-            $event->setUser($user);
-
-            if ($i % 2 == 0) {
-                $event->setEventName(AnalyticalEventType::USER_CREATION);
-                $event->setEventMetadata([
-                    'platform' => 'Live',
-                ]);
-            } else {
-                $event->setEventName(AnalyticalEventType::USER_VERIFICATION);
-            }
-
-            $manager->persist($event);
-
-            if ($i % 2 == 0) {
-                $downloadEvent = new Event();
-                $downloadEvent->setEventName(AnalyticalEventType::DOWNLOAD_PROFILE);
-                $downloadEvent->setEventDatetime(new DateTime());
-                $downloadEvent->setUser($user);
-
-                $platforms = ['Android', 'Windows', 'iOS', 'macOS'];
-                $randomPlatform = $platforms[array_rand($platforms)];
-
-                $downloadEvent->setEventMetadata([
-                    'type' => $randomPlatform,
-                    'platform' => 'Live',
-                ]);
-
-                $manager->persist($downloadEvent);
-            }
-        }
-
         $manager->flush();
     }
 }
