b0b5c055f8e5f1e6537f6066c11b5df4123a9146
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2023-11-15 14:08:17 -0100

    Reset SMS configuration command

---
+++ src/Command/ResetSMSSettingsCommand.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\Setting;
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\QuestionHelper;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+
+#[AsCommand(
+    name: 'reset:smsSettings',
+    description: 'Reset SMS Configuration Settings',
+)]
+class ResetSMSSettingsCommand extends Command
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setName('reset:smsSettings')
+            ->setDescription('Reset SMS Settings')
+            ->addOption('yes', 'y', InputOption::VALUE_NONE, 'Automatically confirm the reset');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        // Check if the --yes option is provided (comes from a controller), then skip the confirmation prompt
+        if (!$input->getOption('yes')) {
+            $helper = $this->getHelper('question');
+            $question = new ConfirmationQuestion('This action will reset the sms configuration settings. [y/N] ', false);
+            /** @var QuestionHelper $helper */
+            if (!$helper->ask($input, $output, $question)) {
+                $output->writeln('Command aborted.');
+                return Command::SUCCESS;
+            }
+        }
+
+        $settings = [
+            ['name' => 'SMS_USERNAME', 'value' => ''],
+            ['name' => 'SMS_USER_ID', 'value' => ''],
+            ['name' => 'SMS_HANDLE', 'value' => ''],
+        ];
+
+        // Begin a database transaction to ensure data consistency
+        $this->entityManager->beginTransaction();
+
+        try {
+            $settingsRepository = $this->entityManager->getRepository(Setting::class);
+
+            foreach ($settings as $settingData) {
+                $name = $settingData['name'];
+                $value = $settingData['value'];
+
+                // Look for all the settings using the name
+                $setting = $settingsRepository->findOneBy(['name' => $name]);
+
+                if ($setting) {
+                    // Update the already existing value
+                    $setting->setValue($value);
+                } else {
+                    // If it doesn't exist, create a new setting from the $setting
+                    $setting = new Setting();
+                    $setting->setName($name);
+                    $setting->setValue($value);
+                    $this->entityManager->persist($setting);
+                }
+            }
+
+            $this->entityManager->flush();
+            $this->entityManager->commit();
+
+            $message = <<<EOL
+
+<info>Success:</info> All the sms configuration settings have been set to the default values.
+<comment>Note:</comment> If you want to reset any another setting please check using this command:
+      <fg=blue>php bin/console reset</>
+EOL;
+
+            // Output the styled message
+            $output->write($message);
+            $output->writeln(['']);
+        } catch (Exception $e) {
+            // Handle any exceptions and roll back in case of an error
+            $this->entityManager->rollback();
+            $output->writeln('An error occurred while resetting settings: ' . $e->getMessage());
+            return Command::FAILURE;
+        }
+
+        return Command::SUCCESS;
+    }
+}
