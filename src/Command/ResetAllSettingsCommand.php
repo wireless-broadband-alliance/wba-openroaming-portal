429c457befc37179a1dc4dc9d3707a5c62f8b162
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2024-05-02 14:41:36 +0000

    reset cloud_flare settings

--- src/Command/ResetAllSettingsCommand.php
+++ src/Command/ResetAllSettingsCommand.php
@@ -61,6 +61,7 @@ class ResetAllSettingsCommand extends Command
 
             ['name' => 'PLATFORM_MODE', 'value' => 'Demo'],
             ['name' => 'USER_VERIFICATION', 'value' => 'OFF'],
+            ['name' => 'CLOUD_FLARE_CHECKER', 'value' => 'OFF'],
 
             ['name' => 'PAGE_TITLE', 'value' => 'OpenRoaming Portal'],
             ['name' => 'CUSTOMER_LOGO', 'value' => '/resources/logos/WBA_20th_logo.png'],
