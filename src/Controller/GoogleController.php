f22aed59cb05b621d4e0a41794d0aad8401788bb
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2024-05-15 09:27:20 +0000

    Fix bug with google authentication when the user cancels it

--- src/Controller/GoogleController.php
+++ src/Controller/GoogleController.php
@@ -87,8 +87,12 @@ class GoogleController extends AbstractController
         // Retrieve the "google" client
         $client = $this->clientRegistry->getClient('google');
 
-        // Get the authorization code from the query parameters
+        // Check if the 'code' parameter is present in the request
         $code = $request->query->get('code');
+        if ($code === null) {
+            $this->addFlash('error', 'Authentication process cancelled.');
+            return $this->redirectToRoute('app_landing');
+        }
 
         // Exchange the authorization code for an access token
         $accessToken = $client->getOAuth2Provider()->getAccessToken('authorization_code', [
