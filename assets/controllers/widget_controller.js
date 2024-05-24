4a7dc559c88f4e7d9ac09738c0bc4cdd069484c7
Dário Santos <dariosantos@tetrapi.pt>
2024-05-24 14:04:38 +0000

    Cleaned widget stimulus controller. Added fields controller

--- assets/controllers/widget_controller.js
+++ assets/controllers/widget_controller.js
@@ -4,42 +4,7 @@ export default class extends Controller {
 	static targets = ["modal"];
 
 	connect() {
-		function checkInputsEnableButtons(buttonId) {
-			document.addEventListener("DOMContentLoaded", function () {
-				const button = document.getElementById(buttonId);
-				const inputs = document.querySelectorAll("input");
-
-				// Store the initial values each input
-				const initialValues = {};
-				inputs.forEach(function (input) {
-					initialValues[input.id] = input.value;
-				});
-
-				function areInputsRestored() {
-					let restored = true;
-					inputs.forEach(function (input) {
-						if (input.value !== initialValues[input.id]) {
-							restored = false;
-						}
-					});
-					return restored;
-				}
-
-				inputs.forEach(function (input) {
-					input.addEventListener("input", function () {
-						if (!areInputsRestored()) {
-							button.removeAttribute("disabled");
-							button.classList.remove("cursor-not-allowed");
-						} else {
-							button.setAttribute("disabled", "disabled");
-							button.classList.add("cursor-not-allowed");
-						}
-					});
-				});
-			});
-		}
-		checkInputsEnableButtons('widgetConfirmAccount');
-
+		super.connect();
 	}
 
 	open() {
