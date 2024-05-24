77aff3f597506a058da23a442bef1abb2428c952
Dário Santos <dariosantos@tetrapi.pt>
2024-05-24 20:36:24 +0000

    Added card controller. Cleaned descriptionCard

---
+++ assets/controllers/card_controller.js
@@ -0,0 +1,19 @@
+import {Controller} from '@hotwired/stimulus';
+
+export default class extends Controller {
+	static targets = ["button", 'info'];
+
+	connect() {
+		super.connect();
+	}
+
+	show_info(){
+		this.infoTarget.classList.remove('hidden');
+		this.infoTarget.classList.add('opacity-100');
+	}
+
+	hide_info(){
+		this.infoTarget.classList.add('hidden');
+		this.infoTarget.classList.remove('opacity-100');
+	}
+}
--- assets/controllers/descriptionCard_controller.js
+++ assets/controllers/descriptionCard_controller.js
@@ -2,27 +2,6 @@ import {Controller} from '@hotwired/stimulus';
 
 export default class extends Controller {
 	connect() {
-		const description_values = document.getElementsByName("description");
-		const description_targets = document.getElementsByName("descriptionIcon");
-
-		description_targets.forEach((description_target, index) => {
-			let timeout; // Initialize a timeout variable
-
-			description_target.addEventListener('mouseover', function handleMouseOver() {
-				// Delay showing the description box for 500 milliseconds (adjust as needed)
-				timeout = setTimeout(() => {
-					description_values[index].classList.remove('hidden');
-					description_values[index].classList.add('opacity-100');
-				}, 500);
-			});
-
-			description_target.addEventListener('mouseout', function handleMouseOut() {
-				clearTimeout(timeout); // Clear the timeout if the user moves the mouse out before the delay
-				description_values[index].classList.add('hidden');
-				description_values[index].classList.remove('opacity-100');
-			});
-		});
-
 		const imageContainers = document.querySelectorAll('.image-container');
 		imageContainers.forEach((container) => {
 			const imagePreview = container.querySelector('[data-image-preview]');
