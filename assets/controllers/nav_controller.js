188b397084c471c239ebe22c68b657cd17e270be
Dário Santos <dariosantos@tetrapi.pt>
2024-05-24 18:17:58 +0000

    Removed user list toggle logic from navbar logic

--- assets/controllers/nav_controller.js
+++ assets/controllers/nav_controller.js
@@ -27,36 +27,6 @@ export default class extends Controller {
 
 		remove_element('errorDisplay');
 		remove_element('successDisplay');
-
-		function initializeDropdown_ActionItems(buttonName, menuName) {
-			const dropdownButtonItems = document.getElementsByName(buttonName);
-			const dropdownMenuItems = document.getElementsByName(menuName);
-
-			// Loop through all elements with the given name and attach event listeners
-			dropdownButtonItems.forEach((button, index) => {
-				button.addEventListener('click', () => {
-					const dropdownMenu = dropdownMenuItems[index];
-					if (dropdownMenu.style.display === 'block') {
-						dropdownMenu.style.display = 'none';
-					} else {
-						dropdownMenu.style.display = 'block';
-					}
-				});
-			});
-
-			// Close the dropdown when clicking outside it
-			document.addEventListener('click', (event) => {
-				dropdownButtonItems.forEach((button, index) => {
-					const dropdownMenu = dropdownMenuItems[index];
-					if (!button.contains(event.target) && !dropdownMenu.contains(event.target)) {
-						dropdownMenu.style.display = 'none';
-					}
-				});
-			});
-		}
-
-		initializeDropdown_ActionItems('itemsDropDownButton', 'itemsDropDown');
-		initializeDropdown_ActionItems('widgetUserButton', 'widgetUser');
 	}
 
 	toggle() {
