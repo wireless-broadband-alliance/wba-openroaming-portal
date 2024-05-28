235151ed22cad1893e6133dccdf402f11b2fd36a
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2024-05-28 10:42:44 +0000

    change name to "OpenRoaming"

--- src/Command/ResetAllSettingsCommand.php
+++ src/Command/ResetAllSettingsCommand.php
@@ -108,7 +108,7 @@ class ResetAllSettingsCommand extends Command
             ['name' => 'SMS_USERNAME', 'value' => ''],
             ['name' => 'SMS_USER_ID', 'value' => ''],
             ['name' => 'SMS_HANDLE', 'value' => ''],
-            ['name' => 'SMS_FROM', 'value' => 'OR_PROVISIONING'],
+            ['name' => 'SMS_FROM', 'value' => 'OpenRoaming'],
             ['name' => 'SMS_TIMER_RESEND', 'value' => '5'],
         ];
 
--- src/DataFixtures/SettingFixture.php
+++ src/DataFixtures/SettingFixture.php
@@ -70,7 +70,7 @@ class SettingFixture extends Fixture
             ['name' => 'SMS_USERNAME', 'value' => ''],
             ['name' => 'SMS_USER_ID', 'value' => ''],
             ['name' => 'SMS_HANDLE', 'value' => ''],
-            ['name' => 'SMS_FROM', 'value' => 'OR_PROVISIONING'],
+            ['name' => 'SMS_FROM', 'value' => 'OpenRoaming'],
             ['name' => 'SMS_TIMER_RESEND', 'value' => '5'],
         ];
 
