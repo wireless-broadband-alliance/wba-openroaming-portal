ab53cada9f8b797cb901c3c42e177873397407e9
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2024-07-18 13:22:54 +0000

    Showing the Setting entity to the ApiResource

--- src/Entity/Setting.php
+++ src/Entity/Setting.php
@@ -2,10 +2,12 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\SettingRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: SettingRepository::class)]
+#[ApiResource]
 class Setting
 {
     #[ORM\Id]
