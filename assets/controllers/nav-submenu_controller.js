68e0590fa3755c4c256a3862dbbefb35908a9d1a
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2024-07-18 12:04:17 +0000

    reverting some stuff
    I hate doing front end
    :3

--- assets/controllers/nav-submenu_controller.js
+++ assets/controllers/nav-submenu_controller.js
@@ -2,29 +2,22 @@ import {Controller} from '@hotwired/stimulus';
 
 export default class extends Controller {
 
-	static targets = ["button", "container", "icon"];
+    static targets = ["button", "container", "icon"];
 
-	connect() {
-		super.connect();
-	}
+    connect() {
+        super.connect();
+    }
 
-	toggle() {
-		this.containerTarget.classList.toggle('hidden');
-		if (this.hasIconTarget) {
-			this.iconTarget.classList.toggle('rotate-90');
-		}
-	}
+    toggle() {
+        this.containerTarget.classList.toggle('hidden');
+        if (this.hasIconTarget) {
+            this.iconTarget.classList.toggle('rotate-90');
+        }
+    }
 
-	toggleReverse() {
-		this.containerTarget.classList.toggle('hidden');
-		if (this.hasIconTarget) {
-			this.iconTarget.classList.toggle('-rotate-90');
-		}
-	}
-
-	lost_focus() {
-		if (!this.containerTarget.matches(":hover")) {
-			this.containerTarget.classList.add('hidden');
-		}
-	}
+    lost_focus() {
+        if (!this.containerTarget.matches(":hover")) {
+            this.containerTarget.classList.add('hidden');
+        }
+    }
 }
