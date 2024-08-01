7329602529c0c9c4cd2133303d9f090a60b32b9f
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2024-08-01 12:29:35 +0000

    Local Authentication and token generation API done

--- src/Api/V1/Controller/LocalAuthController.php
+++ src/Api/V1/Controller/LocalAuthController.php
@@ -2,14 +2,8 @@
 
 namespace App\Api\V1\Controller;
 
-use App\Entity\User;
-use App\Entity\UserExternalAuth;
-use App\Enum\AnalyticalEventType;
-use App\Enum\UserProvider;
 use App\Repository\UserRepository;
-use App\Service\EventActions;
-use DateTime;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Service\JWTTokenGenerator;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -19,6 +13,19 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class LocalAuthController extends AbstractController
 {
+    private UserRepository $userRepository;
+    private UserPasswordHasherInterface $passwordHasher;
+    private jwtTokenGenerator $tokenGenerator;
+
+    public function __construct(
+        UserRepository $userRepository,
+        UserPasswordHasherInterface $passwordHasher,
+        jwtTokenGenerator $tokenGenerator,
+    ) {
+        $this->userRepository = $userRepository;
+        $this->passwordHasher = $passwordHasher;
+        $this->tokenGenerator = $tokenGenerator;
+    }
 
     /**
      * @throws Exception
@@ -26,7 +33,33 @@ class LocalAuthController extends AbstractController
     #[Route('/api/v1/auth/local', name: 'api_auth_local', methods: ['POST'])]
     public function authLocal(Request $request): JsonResponse
     {
-        return $this->json('Eu sou um rabo');
-    }
+        $data = json_decode($request->getContent(), true, 512, JSON_THROW_ON_ERROR);
 
+        if (!isset($data['uuid'], $data['password'])) {
+            return new JsonResponse(['error' => 'Invalid data'], 404);
+        }
+
+        $user = $this->userRepository->findOneBy(['uuid' => $data['uuid']]);
+
+        if (!$user || !$this->passwordHasher->isPasswordValid($user, $data['password'])) {
+            return new JsonResponse(['error' => 'Invalid credentials - Missing User'], 404);
+        }
+
+        $token = $this->tokenGenerator->generateToken($user);
+
+        $responseData = [
+            'token' => $token,
+            'user' => [
+                'uuid' => $user->getUuid(),
+                'email' => $user->getEmail(),
+                'roles' => $user->getRoles(),
+                'first_name' => $user->getFirstName(),
+                'last_name' => $user->getLastName(),
+                'isVerified' => $user->isVerified(),
+                'createdAt' => $user->getCreatedAt(),
+            ]
+        ];
+
+        return new JsonResponse($responseData, 200);
+    }
 }
