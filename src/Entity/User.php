06edc7d8fb15c23cc5e6401f11713876088e5982
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2024-09-19 09:38:03 +0000

    fix psr12 warning limit characters for the message

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -829,8 +829,9 @@ use Symfony\Component\Validator\Constraints as Assert;
                                     'properties' => [
                                         'success' => [
                                             'type' => 'string',
-                                            'example' => 'We have sent a new code to: +1234567890. 
-                                            You have 3 attempt(s) left.',
+                                            // phpcs:disable Generic.Files,LineLenght.TooLong
+                                            'example' => 'We have sent a new code to: +1234567890. You have 3 attempt(s) left.',
+                                            // phpcs:enable
                                         ],
                                     ],
                                 ],
