2ceb9d27ffe5bb2f972f87eae41121f13124570e
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2024-10-04 08:55:13 +0000

    Fix messages verification setup before downloading a profile

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -91,7 +91,7 @@ class ProfileController extends AbstractController
         ) {
             $this->addFlash(
                 'error',
-                'Your account is not verified. Please, for more information contact our support.'
+                'Your account is not verified to download a profile!.'
             );
             return $this->redirectToRoute('app_landing');
         }
@@ -180,7 +180,7 @@ class ProfileController extends AbstractController
         ) {
             $this->addFlash(
                 'error',
-                'Your account is not verified. Please, for more information contact our support.'
+                'Your account is not verified to download a profile!.'
             );
             return $this->redirectToRoute('app_landing');
         }
@@ -314,7 +314,7 @@ class ProfileController extends AbstractController
         ) {
             $this->addFlash(
                 'error',
-                'Your account is not verified. Please, for more information contact our support.'
+                'Your account is not verified to download a profile!.'
             );
             return $this->redirectToRoute('app_landing');
         }
