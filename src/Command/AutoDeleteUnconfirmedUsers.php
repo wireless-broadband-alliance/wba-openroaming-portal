67f0edf6c25f24b26c1dd7b7d0af3833080ca526
rodrigoresendesmoura <rodrigoresendesmoura@tetrapi.pt>
2024-10-29 14:01:42 -0100

    cron command for delete unconfirmed users

---
+++ src/Command/AutoDeleteUnconfirmedUsers.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\Setting;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+    name: 'app:delete_unconfirmed_users',
+    description: 'Allocate providers info from User Entity to the UserExternalAuth Entity',
+)]
+
+class AutoDeleteUnconfirmedUsers extends Command
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        parent::__construct();
+        $this->entityManager = $entityManager;
+    }
+
+    public function delete_unconfirmed_users() {
+        $userRepository = $this->entityManager->getRepository(User::class);
+        $settingsRepository = $this->entityManager->getRepository(Setting::class);
+        $users = $userRepository->findAll();
+        $settingTime = $settingsRepository->findAllIn(['USER_DELETE_TIME']);
+        foreach ($users as $user) {
+            $timeString = $settingTime[0]->getValue();
+            $time = (int)$timeString;
+            $limitTime = $user->getCreatedAt();
+            $limitTime->modify("+ {$time} hours");
+            $realTime = new \DateTime();
+            if ( !$user->isVerified()) {
+                if ($limitTime < $realTime) {
+                    $this->entityManager->remove($user);
+                }
+            }
+            $this->entityManager->flush();
+        }
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->delete_unconfirmed_users();
+        $output->writeln('Notificações enviadas com sucesso.');
+
+        return Command::SUCCESS;
+    }
+}
