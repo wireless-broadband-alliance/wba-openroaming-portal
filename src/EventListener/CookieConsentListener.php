b973bc992c3b7f73fd8e80ba918237cd902aafe5
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2024-11-18 11:38:59 -0100

    Fix possible crash with empty cookies

--- src/EventListener/CookieConsentListener.php
+++ src/EventListener/CookieConsentListener.php
@@ -2,6 +2,7 @@
 
 namespace App\EventListener;
 
+use DateTime;
 use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
@@ -24,14 +25,19 @@ class CookieConsentListener
     {
         $request = $this->requestStack->getCurrentRequest();
         // Check if cookies are available
-        if ($request !== null && $request->cookies !== null) {
-            // Get cookie preferences and terms acceptance
-            $preferences = json_decode(
-                $request->cookies->get('cookie_preferences', '{}'),
-                true,
-                512,
-                JSON_THROW_ON_ERROR
-            );
+        if ($request !== null && $request->cookies->has('cookie_preferences')) {
+            try {
+                // Get cookie preferences and terms acceptance
+                $preferences = json_decode(
+                    $request->cookies->get('cookie_preferences'),
+                    true,
+                    512,
+                    JSON_THROW_ON_ERROR
+                );
+            } catch (\JsonException $e) {
+                // Handle invalid JSON gracefully
+                $preferences = [];
+            }
         }
 
         $response = $event->getResponse();
@@ -46,7 +52,7 @@ class CookieConsentListener
      */
     private function setNecessaryCookie($response): void
     {
-        $necessaryExpiration = (new \DateTime())->modify('+1 year');
+        $necessaryExpiration = (new DateTime())->modify('+1 year');
         $response->headers->setCookie(
             new Cookie(
                 'necessary_cookie',
