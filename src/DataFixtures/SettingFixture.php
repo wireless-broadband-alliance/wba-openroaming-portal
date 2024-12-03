8cd36cf489f5ee4c71e912427e99a6766977f574
rodrigoresendesmoura <rodrigoresendesmoura@tetrapi.pt>
2024-12-03 13:55:31 -0100

    migration

--- src/DataFixtures/SettingFixture.php
+++ src/DataFixtures/SettingFixture.php
@@ -70,8 +70,8 @@ class SettingFixture extends Fixture
             ['name' => 'PRIVACY_POLICY', 'value' => 'LINK'],
             ['name' => 'TOS_LINK', 'value' => 'https://wballiance.com/openroaming/toc/'],
             ['name' => 'PRIVACY_POLICY_LINK', 'value' => 'https://wballiance.com/openroaming/privacy-policy'],
-            ['name' => 'TOS_EDITOR', 'value' => null],
-            ['name' => 'PRIVACY_POLICY_EDITOR', 'value' => null],
+            ['name' => 'TOS_EDITOR', 'value' => 'TEXT_EDITOR'],
+            ['name' => 'PRIVACY_POLICY_EDITOR', 'value' => 'TEXT_EDITOR'],
 
             ['name' => 'VALID_DOMAINS_GOOGLE_LOGIN', 'value' => ''],
             ['name' => 'PROFILES_ENCRYPTION_TYPE_IOS_ONLY', 'value' => 'WPA2'],
