99b1d26183de801e0a01fee4a3dda8550fdae324
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2025-01-06 10:59:27 -0100

    Composer require thenetworg/oauth2-azure
    New bundle instalation - Azure
    Remove unsed bundle - Microsoft
    Fix symfony vulnerabilities - CVE's
    Following cve's fixed with composer update:
     - CVE-2024-56408
     - CVE-2024-56365
     - CVE-2024-56366
     - CVE-2024-56409
     - CVE-2024-56410
     - CVE-2024-56411
     - CVE-2024-56412

--- src/DataFixtures/SettingFixture.php
+++ src/DataFixtures/SettingFixture.php
@@ -8,7 +8,7 @@ use Doctrine\Persistence\ObjectManager;
 
 class SettingFixture extends Fixture
 {
-    public function load(ObjectManager $manager)
+    public function load(ObjectManager $manager): void
     {
         $settings = [
             ['name' => 'RADIUS_REALM_NAME', 'value' => 'EditMe'],
