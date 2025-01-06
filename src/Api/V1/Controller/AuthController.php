76c6848c1917ad089e80175be54c9cf4eae9faf8
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2025-01-06 11:31:51 -0100

    MIcrosoft api endpoint implementation
    Fix call of miss typing controllers
    Endpoint documentation creation

--- src/Api/V1/Controller/AuthController.php
+++ src/Api/V1/Controller/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Api\V1\Controller;
 
 use App\Api\V1\BaseResponse;
+use App\Controller\GoogleController;
 use App\Controller\MicrosoftController;
 use App\Entity\User;
 use App\Entity\UserExternalAuth;
@@ -16,6 +17,7 @@ use App\Service\UserStatusChecker;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
+use JsonException;
 use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 use OneLogin\Saml2\Auth;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -36,7 +38,8 @@ class AuthController extends AbstractController
     private jwtTokenGenerator $tokenGenerator;
     private CaptchaValidator $captchaValidator;
     private EntityManagerInterface $entityManager;
-    private MicrosoftController $googleController;
+    private GoogleController $googleController;
+    private MicrosoftController $microsoftController;
     private UserStatusChecker $userStatusChecker;
     private EventActions $eventActions;
 
@@ -46,7 +49,8 @@ class AuthController extends AbstractController
      * @param JWTTokenGenerator $tokenGenerator
      * @param CaptchaValidator $captchaValidator
      * @param EntityManagerInterface $entityManager
-     * @param MicrosoftController $googleController
+     * @param GoogleController $googleController
+     * @param MicrosoftController $microsoftController
      * @param UserStatusChecker $userStatusChecker
      * @param EventActions $eventActions
      */
@@ -56,7 +60,8 @@ class AuthController extends AbstractController
         jwtTokenGenerator $tokenGenerator,
         CaptchaValidator $captchaValidator,
         EntityManagerInterface $entityManager,
-        MicrosoftController $googleController,
+        GoogleController $googleController,
+        MicrosoftController $microsoftController,
         UserStatusChecker $userStatusChecker,
         EventActions $eventActions,
     ) {
@@ -66,6 +71,7 @@ class AuthController extends AbstractController
         $this->captchaValidator = $captchaValidator;
         $this->entityManager = $entityManager;
         $this->googleController = $googleController;
+        $this->microsoftController = $microsoftController;
         $this->userStatusChecker = $userStatusChecker;
         $this->eventActions = $eventActions;
     }
@@ -85,7 +91,7 @@ class AuthController extends AbstractController
     {
         try {
             $data = json_decode($request->getContent(), true, 512, JSON_THROW_ON_ERROR);
-        } catch (\JsonException $e) {
+        } catch (JsonException) {
             return (new BaseResponse(400, null, 'Invalid JSON format'))->toResponse(); # Bad Request Response
         }
 
@@ -270,7 +276,7 @@ class AuthController extends AbstractController
     {
         try {
             $data = json_decode($request->getContent(), true, 512, JSON_THROW_ON_ERROR);
-        } catch (\JsonException $e) {
+        } catch (JsonException) {
             return (new BaseResponse(400, null, 'Invalid JSON format'))->toResponse();
         }
 
@@ -279,7 +285,7 @@ class AuthController extends AbstractController
         }
 
         try {
-            $user = $this->googleController->fetchUserFromMicrosoft($data['code']);
+            $user = $this->googleController->fetchUserFromGoogle($data['code']);
             if ($user === null) {
                 return (new BaseResponse(400, null, 'This code is not associated with a google account.'))->toResponse(
                 );
@@ -291,7 +297,65 @@ class AuthController extends AbstractController
             }
 
             // Authenticate the user using custom Google authentication function already on the project
-            $this->googleController->authenticateUserMicrosoft($user);
+            $this->googleController->authenticateUserGoogle($user);
+
+            $token = $this->tokenGenerator->generateToken($user);
+
+            $formattedUserData = $user->toApiResponse(['token' => $token]);
+
+            // Defines the Event to the table
+            $eventMetadata = [
+                'ip' => $request->getClientIp(),
+                'uuid' => $user->getUuid(),
+            ];
+
+            $this->eventActions->saveEvent(
+                $user,
+                AnalyticalEventType::AUTH_GOOGLE_API,
+                new DateTime(),
+                $eventMetadata
+            );
+
+            return (new BaseResponse(200, $formattedUserData, null))->toResponse();
+        } catch (IdentityProviderException) {
+            // Handle OAuth identity provider-specific errors
+            return (new BaseResponse(500, null, 'Authentication failed'))->toResponse();
+        } catch (Exception) {
+            // Handle any other general errors
+            return (new BaseResponse(500, null, 'An error occurred'))->toResponse();
+        }
+    }
+
+    #[Route('/api/v1/auth/microsoft', name: 'api_auth_microsoft', methods: ['POST'])]
+    public function authMicrosoft(Request $request): JsonResponse
+    {
+        try {
+            $data = json_decode($request->getContent(), true, 512, JSON_THROW_ON_ERROR);
+        } catch (JsonException) {
+            return (new BaseResponse(400, null, 'Invalid JSON format'))->toResponse();
+        }
+
+        if (!isset($data['code'])) {
+            return (new BaseResponse(400, null, 'Missing authorization code!'))->toResponse();
+        }
+
+        try {
+            $user = $this->microsoftController->fetchUserFromMicrosoft($data['code']);
+            if ($user === null) {
+                return (new BaseResponse(
+                    400,
+                    null,
+                    'This code is not associated with a microsoft account.'
+                ))->toResponse();
+            }
+
+            $statusCheckerResponse = $this->userStatusChecker->checkUserStatus($user);
+            if ($statusCheckerResponse !== null) {
+                return $statusCheckerResponse->toResponse();
+            }
+
+            // Authenticate the user using custom Google authentication function already on the project
+            $this->microsoftController->authenticateUserMicrosoft($user);
 
             $token = $this->tokenGenerator->generateToken($user);
 
