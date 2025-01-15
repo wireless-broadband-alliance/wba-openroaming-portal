4713f466efe9bd753617b9a9cf39c664d7aca9c0
rodrigoresendesmoura <rodrigoresendesmoura@tetrapi.pt>
2025-01-15 10:27:57 -0100

    rmove ckeditor

--- config/bundles.php
+++ config/bundles.php
@@ -28,6 +28,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Misd\PhoneNumberBundle\MisdPhoneNumberBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
-    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     Eckinox\TinymceBundle\TinymceBundle::class => ['all' => true],
 ];
