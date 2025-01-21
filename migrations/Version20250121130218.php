abd4b5b6249a3fae0980098d61c671ac08272105
rodrigoresendesmoura <rodrigoresendesmoura@tetrapi.pt>
2025-01-21 17:02:47 -0100

    migration

---
+++ migrations/Version20250121130218.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250121130218 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE Notification (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, lastNotification DATETIME NOT NULL, type VARCHAR(255) DEFAULT NULL, INDEX IDX_A765AD32A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE Notification ADD CONSTRAINT FK_A765AD32A76ED395 FOREIGN KEY (user_id) REFERENCES User (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE Notification DROP FOREIGN KEY FK_A765AD32A76ED395');
+        $this->addSql('DROP TABLE Notification');
+    }
+}
