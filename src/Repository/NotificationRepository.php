7ff00ae5ebe4f7322af8f4019fb8185ca7366374
rodrigoresendesmoura <rodrigoresendesmoura@tetrapi.pt>
2025-01-21 17:22:02 -0100

    phpstan

--- src/Repository/NotificationRepository.php
+++ src/Repository/NotificationRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Notification;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
