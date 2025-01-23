c08227471f74d0567043d691c9b4bf900bba0a46
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2025-01-23 13:19:14 -0100

    Ensure only one active SAML Provider at a time
    This command will not remove any previous registered providers, it only desactives any previous one created and generate a new one has the default

--- src/Command/SetSamlProviderCommand.php
+++ src/Command/SetSamlProviderCommand.php
@@ -3,7 +3,10 @@
 namespace App\Command;
 
 use App\Entity\SamlProvider;
+use App\Entity\UserExternalAuth;
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
+use InvalidArgumentException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputInterface;
@@ -42,32 +45,79 @@ class SetSamlProviderCommand extends Command
 
         if (empty($name)) {
             $output->writeln('<error>Name cannot be empty!</error>');
+
             return self::FAILURE;
         }
 
         try {
             $this->createAndPersistSamlProvider($name);
             $output->writeln('SAML Provider data has been set!');
+
             return self::SUCCESS;
-        } catch (\Exception $e) {
-            $output->writeln('<error>Error: ' . $e->getMessage() . '</error>');
+        } catch (Exception $e) {
+            $output->writeln('<error>Error: '.$e->getMessage().'</error>');
+
             return self::FAILURE;
         }
     }
 
+    /**
+     * @throws Exception
+     */
     private function createAndPersistSamlProvider(string $name): void
     {
-        $samlProvider = new SamlProvider();
+        $idpEntityId = $this->parameterBag->get('app.saml_idp_entity_id');
+        $idpSsoUrl = $this->parameterBag->get('app.saml_idp_sso_url');
+        $idpX509Cert = $this->parameterBag->get('app.saml_idp_x509_cert');
+        $spEntityId = $this->parameterBag->get('app.saml_sp_entity_id');
+        $spAcsUrl = $this->parameterBag->get('app.saml_sp_acs_url');
 
-        $samlProvider->setName($name);
-        $samlProvider->setIdpEntityId($this->parameterBag->get('app.saml_idp_entity_id'));
-        $samlProvider->setIdpSsoUrl($this->parameterBag->get('app.saml_idp_sso_url'));
-        $samlProvider->setIdpX509Cert($this->parameterBag->get('app.saml_idp_x509_cert'));
-        $samlProvider->setSpEntityId($this->parameterBag->get('app.saml_sp_entity_id'));
-        $samlProvider->setSpAcsUrl($this->parameterBag->get('app.saml_sp_acs_url'));
-        $samlProvider->setActive(true);
+        // Validate required SAML fields
+        if (empty($idpEntityId) || empty($idpSsoUrl) || empty($idpX509Cert) || empty($spEntityId) || empty($spAcsUrl)) {
+            throw new InvalidArgumentException('One or more SAML configuration values are missing!');
+        }
 
-        $this->entityManager->persist($samlProvider);
-        $this->entityManager->flush();
+        $this->entityManager->beginTransaction(); // Begin a database transaction for atomicity
+
+        try {
+            // Ensure only 1 provider can be active:
+            // Find the currently active provider and deactivate it
+            $activeProvider = $this->entityManager->getRepository(SamlProvider::class)->findOneBy(['isActive' => true]);
+            if ($activeProvider) {
+                $activeProvider->setActive(false);
+                $this->entityManager->persist($activeProvider);
+            }
+
+            // Create and persist the new provider
+            $samlProvider = new SamlProvider();
+            $samlProvider->setName($name);
+            $samlProvider->setIdpEntityId($idpEntityId);
+            $samlProvider->setIdpSsoUrl($idpSsoUrl);
+            $samlProvider->setIdpX509Cert($idpX509Cert);
+            $samlProvider->setSpEntityId($spEntityId);
+            $samlProvider->setSpAcsUrl($spAcsUrl);
+            $samlProvider->setActive(true);
+
+            $this->entityManager->persist($samlProvider);
+            $this->entityManager->flush();
+
+            // Reassign UserExternalAuth entities associated with the old provider to the new provider
+            if ($activeProvider) {
+                $userExternalAuths = $this->entityManager->getRepository(UserExternalAuth::class)
+                    ->findBy(['samlProvider' => $activeProvider]); // Find all users linked to the old provider
+
+                foreach ($userExternalAuths as $userExternalAuth) {
+                    $userExternalAuth->setSamlProvider($samlProvider); // Link them to the new provider
+                    $this->entityManager->persist($userExternalAuth);
+                }
+
+                $this->entityManager->flush();
+            }
+
+            $this->entityManager->commit();
+        } catch (Exception $e) {
+            $this->entityManager->rollback();
+            throw $e;
+        }
     }
 }
