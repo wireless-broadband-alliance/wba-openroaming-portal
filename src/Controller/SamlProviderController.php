d8ce2d91c76e04c51158be96761b3d46b38d33a2
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2025-01-27 10:21:36 -0100

    Starting new template for SamlProvider's

---
+++ src/Controller/SamlProviderController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\SettingRepository;
+use App\Repository\UserRepository;
+use App\Service\GetSettings;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class SamlProviderController extends AbstractController
+{
+    public function __construct(
+        private readonly UserRepository $userRepository,
+        private readonly settingRepository $settingRepository,
+        private readonly GetSettings $getSettings,
+        // private readonly EntityManagerInterface $entityManager,
+    )
+    {
+    }
+
+    #[Route('dashboard/saml-provider', name: 'admin_dashboard_saml_provider')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function index(): Response
+    {
+        // Get the current logged-in user (admin)
+        /** @var User $currentUser */
+        $currentUser = $this->getUser();
+        $data = $this->getSettings->getSettings($this->userRepository, $this->settingRepository);
+
+        return $this->render('admin/settings_actions.html.twig', [
+            'data' => $data,
+            'current_user' => $currentUser,
+        ]);
+    }
+}
