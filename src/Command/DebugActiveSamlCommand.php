55fa98c9e9b45cbb79ee559a53b871a632149f74
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2025-01-30 10:08:28 -0100

    Update DebugCommand for symfony 6.4

--- src/Command/DebugActiveSamlCommand.php
+++ src/Command/DebugActiveSamlCommand.php
@@ -4,24 +4,21 @@ namespace App\Command;
 
 use App\Service\SamlActiveProviderService;
 use OneLogin\Saml2\Error;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+#[AsCommand(
+    name: 'app:debug-active-saml',
+    description: 'Debug Current Active SAML configuration.'
+)]
 class DebugActiveSamlCommand extends Command
 {
-    protected static $defaultName = 'app:debug-active-saml';
-
-
     public function __construct(
         private readonly SamlActiveProviderService $service
     ) {
         parent::__construct();
-    }
-
-    protected function configure(): void
-    {
-        $this->setDescription('Debug Current Active SAML configuration.');
     }
 
     /**
