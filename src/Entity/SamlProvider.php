0f47677057dd009d1e0d3f916e6098a6b44a07e0
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2025-02-04 16:13:32 -0100

    Add new validator for duplicated field before insert new Provider

--- src/Entity/SamlProvider.php
+++ src/Entity/SamlProvider.php
@@ -3,12 +3,14 @@
 namespace App\Entity;
 
 use App\Repository\SamlProviderRepository;
+use App\Validator\UniqueField;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: SamlProviderRepository::class)]
+#[UniqueField]
 class SamlProvider
 {
     #[ORM\Id]
--- src/Form/SamlProviderType.php
+++ src/Form/SamlProviderType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\SamlProvider;
 use App\Validator\CamelCase;
+use App\Validator\UniqueField;
+use App\Validator\UniqueSamlProvider;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -21,13 +23,15 @@ class SamlProviderType extends AbstractType
                 'label' => 'Provider Name',
                 'constraints' => [
                     new NotBlank(),
-                    new CamelCase()// Apply the custom noSpecialCharacter validator
+                    new CamelCase(), // Apply the custom noSpecialCharacter validator
+                    new UniqueField(['field' => 'name'])
                 ],
             ])
             ->add('idpEntityId', TextType::class, [
                 'label' => 'SAML IDP Entity ID',
                 'constraints' => [
                     new NotBlank(),
+                    new UniqueField(['field' => 'idpEntityId'])
                 ],
             ])
             ->add('idpSsoUrl', TextType::class, [
@@ -38,6 +42,7 @@ class SamlProviderType extends AbstractType
                         'message' => 'The value {{ value }} is not a valid URL.',
                         'protocols' => ['http', 'https'],
                     ]),
+                    new UniqueField(['field' => 'idpSsoUrl'])
                 ],
             ])
             ->add('spAcsUrl', TextType::class, [
@@ -48,12 +53,14 @@ class SamlProviderType extends AbstractType
                         'message' => 'The value {{ value }} is not a valid URL.',
                         'protocols' => ['http', 'https'],
                     ]),
+                    new UniqueField(['field' => 'spAcsUrl'])
                 ],
             ])
             ->add('idpX509Cert', TextareaType::class, [
                 'label' => 'SAML IDP X509 Certificate',
                 'constraints' => [
                     new NotBlank(),
+                    new UniqueField(['field' => 'idpX509Cert'])
                 ],
             ]);
     }
