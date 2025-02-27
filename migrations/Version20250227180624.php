dcbbfd19323066e5e216aa871176178993522143
rodrigoresendesmoura <rodrigoresendesmoura@tetrapi.pt>
2025-02-27 17:32:18 -0100

    migration

--- migrations/Version20250227180624.php
+++ migrations/Version20250227180624.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250226120844 extends AbstractMigration
+final class Version20250227180624 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,14 @@ final class Version20250226120844 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE User CHANGE twoFAtype twoFAtype INT NOT NULL, CHANGE twoFAcodeIsActive twoFAcodeIsActive TINYINT(1) NOT NULL DEFAULT 0');
+        $this->addSql('UPDATE User SET twoFAcodeIsActive = 0 WHERE twoFAcodeIsActive IS NULL');
+        $this->addSql('ALTER TABLE User CHANGE twoFAtype twoFAtype INT NOT NULL, CHANGE twoFAcodeIsActive twoFAcodeIsActive TINYINT(1) DEFAULT 0 NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('UPDATE User SET twoFAcodeIsActive = NULL WHERE twoFAcodeIsActive = 0');
         $this->addSql('ALTER TABLE User CHANGE twoFAtype twoFAtype VARCHAR(255) DEFAULT \'0\', CHANGE twoFAcodeIsActive twoFAcodeIsActive TINYINT(1) DEFAULT NULL');
     }
 }
