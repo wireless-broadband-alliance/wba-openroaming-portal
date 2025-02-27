aa022e639c7e53850a947c939dc3ee83c9049811
rodrigoresendesmoura <rodrigoresendesmoura@tetrapi.pt>
2025-02-27 15:04:33 -0100

    migrations changes

--- migrations/Version20250226120844.php
+++ migrations/Version20250226120844.php
@@ -20,7 +20,7 @@ final class Version20250226120844 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE User CHANGE twoFAtype twoFAtype INT NOT NULL, CHANGE twoFAcodeIsActive twoFAcodeIsActive TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE User CHANGE twoFAtype twoFAtype INT NOT NULL, CHANGE twoFAcodeIsActive twoFAcodeIsActive TINYINT(1) NOT NULL DEFAULT 0');
     }
 
     public function down(Schema $schema): void
