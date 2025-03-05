4410c4c46f1c4b755653ad3c3142f6df6c65886b
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2025-03-05 10:46:59 -0100

    Revert to original stimulus implementation
    Im stucked with this
    Idk what to do ;-;

--- assets/controllers/radio_controller.js
+++ assets/controllers/radio_controller.js
@@ -1,7 +1,7 @@
 import {Controller} from "@hotwired/stimulus";
 
 export default class extends Controller {
-    static targets = ["platformMode", "userVerification", "on", "off"];
+    static targets = ["on", "off"];
 
     connect() {
         super.connect();
@@ -21,52 +21,5 @@ export default class extends Controller {
             this.onTarget.classList.add("hidden");
             this.offTarget.classList.remove("hidden");
         }
-    }
-
-    togglePlatformMode(event) {
-        // Get selected PLATFORM_MODE value
-        const platformMode = this.platformModeTargets.find(input => input.checked)?.value;
-        console.log("PLATFORM_MODE:", platformMode);
-
-        // Get selected USER_VERIFICATION value
-        const userVerification = this.userVerificationTargets.find(input => input.checked)?.value;
-        console.log("USER_VERIFICATION:", userVerification);
-
-        // Handle SVG visibility for PLATFORM_MODE
-        this.platformModeTargets.forEach(input => {
-            const parentElement = input.closest('label'); // Parent label
-            const svgElement = parentElement.querySelector('.custom-radio'); // SVG container
-
-            if (svgElement) {
-                if (input.checked) {
-                    svgElement.classList.remove('hidden'); // Show SVG for selected
-                } else {
-                    svgElement.classList.add('hidden'); // Hide SVG for non-selected
-                }
-            }
-        });
-
-        // Handle USER_VERIFICATION based on PLATFORM_MODE
-        this.userVerificationTargets.forEach(input => {
-            const parentElement = input.closest('label'); // Parent label
-            const svgElement = parentElement.querySelector('.custom-radio'); // SVG container
-
-            if (platformMode === 'Live') {
-                if (input.value === 'ON') {
-                    input.checked = true; // Force "ON" to be selected
-                    svgElement.classList.remove('hidden'); // Show "ON" SVG
-                } else {
-                    input.checked = false; // Prevent "OFF" from being selected
-                    svgElement.classList.add('hidden'); // Hide "OFF" SVG
-                }
-            } else if (platformMode === 'Demo') {
-                // Handle SVG visibility based on input selection for Demo
-                if (input.checked) {
-                    svgElement.classList.remove('hidden'); // Show selected SVG
-                } else {
-                    svgElement.classList.add('hidden'); // Hide non-selected SVG
-                }
-            }
-        });
     }
 }
