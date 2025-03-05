148f1b521476b96ef68bb81779413c52132c255e
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2025-03-05 12:03:08 -0100

    Rework debug command to required a $name parameters for the samlProvider

--- src/Command/DebugActiveSamlCommand.php
+++ src/Command/DebugActiveSamlCommand.php
@@ -2,21 +2,23 @@
 
 namespace App\Command;
 
-use App\Service\SamlActiveProviderService;
+use App\Service\SamlProviderResolverService;
 use OneLogin\Saml2\Error;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
 
 #[AsCommand(
     name: 'app:debug-active-saml',
-    description: 'Debug Current Active SAML configuration.'
+    description: 'Debug a specific SAML configuration by name.'
 )]
 class DebugActiveSamlCommand extends Command
 {
     public function __construct(
-        private readonly SamlActiveProviderService $samlActiveProvider
+        private readonly SamlProviderResolverService $samlActiveProvider
     ) {
         parent::__construct();
     }
@@ -26,9 +28,28 @@ class DebugActiveSamlCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $config = $this->samlActiveProvider->getActiveSamlProvider();
-        $output->writeln('Resolved Configuration:');
-        $output->writeln(print_r($config, true));
+        // Ask for the name
+        /** @var QuestionHelper $helper */
+        $helper = $this->getHelper('question');
+        $question = new Question('Please enter the name of the SAML provider to debug: ');
+        $name = $helper->ask($input, $output, $question);
+
+        if (empty($name)) {
+            $output->writeln('<error>The name cannot be empty. Exiting command.</error>');
+            return Command::FAILURE;
+        }
+
+        $output->writeln(sprintf('<info>Fetching SAML configuration for provider: %s</info>', $name));
+
+        try {
+            $config = $this->samlActiveProvider->authSamlProviderByName($name);
+
+            $output->writeln('Resolved Configuration:');
+            $output->writeln(print_r($config, true));
+        } catch (Error $e) {
+            $output->writeln(sprintf('<error>Failed to fetch SAML configuration: %s</error>', $e->getMessage()));
+            return Command::FAILURE;
+        }
 
         return Command::SUCCESS;
     }
---
+++ src/Service/SamlProviderResolverService.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Service;
+
+use App\Repository\SamlProviderRepository;
+use OneLogin\Saml2\Auth;
+use OneLogin\Saml2\Error;
+use RuntimeException;
+
+class SamlProviderResolverService
+{
+    private ?Auth $samlAuth = null;
+
+    public function __construct(
+        private readonly SamlProviderRepository $repository
+    ) {
+    }
+
+    /**
+     * @throws Error
+     */
+    public function authSamlProviderByName(string $samlProviderName): Auth
+    {
+        if (!$this->samlAuth instanceof Auth) {
+            // Fetch active provider
+            $samlProvider = $this->repository->findOneBy([
+                'name' => $samlProviderName,
+                'deletedAt' => null,
+                'isActive' => true,
+                'isLDAPActive' => true,
+            ]);
+
+            if (!$samlProvider) {
+                throw new RuntimeException(
+                    sprintf(
+                        'No active SAML provider found for the name "%s". ' .
+                        'Please make sure you have an LDAPCredential associated with this SAML provider and that ' .
+                        'the SAML provider is active.',
+                        $samlProviderName
+                    )
+                );
+            }
+
+            // Generate settings dynamically
+            $settings = [
+                'sp' => [
+                    'entityId' => $samlProvider->getSpEntityId(),
+                    'assertionConsumerService' => ['url' => $samlProvider->getSpAcsUrl()],
+                ],
+                'idp' => [
+                    'entityId' => $samlProvider->getIdpEntityId(),
+                    'singleSignOnService' => ['url' => $samlProvider->getIdpSsoUrl()],
+                    'x509cert' => $samlProvider->getIdpX509Cert(),
+                ],
+            ];
+
+            $this->samlAuth = new Auth($settings);
+        }
+        return $this->samlAuth;
+    }
+}
