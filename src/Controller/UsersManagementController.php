4ecbd1123f806a40d6f2539ddc1057823fc4e562
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2025-03-27 10:08:07 -0100

    Fix contact email bug with missing paramater on admin password reset

--- src/Controller/UsersManagementController.php
+++ src/Controller/UsersManagementController.php
@@ -413,6 +413,7 @@ class UsersManagementController extends AbstractController
 
             if ($user->getEmail()) {
                 $supportTeam = $data['title']['value'];
+                $contactEmail = $data['contactEmail']['value'];
                 // Send email
                 $email = new Email()
                     ->from(new Address($emailSender, $nameSender))
@@ -421,7 +422,12 @@ class UsersManagementController extends AbstractController
                     ->html(
                         $this->renderView(
                             'email/user_password.html.twig',
-                            ['password' => $newPassword, 'isNewUser' => false, 'supportTeam' => $supportTeam]
+                            [
+                                'password' => $newPassword,
+                                'isNewUser' => false,
+                                'supportTeam' => $supportTeam,
+                                'contactEmail' => $contactEmail
+                            ]
                         )
                     );
                 $mailer->send($email);
