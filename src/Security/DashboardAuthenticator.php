62c9834995c5ae258371f5966e360ccdda29c7a0
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2025-05-02 16:37:48 +0000

    Fix rector pipeline

--- src/Security/DashboardAuthenticator.php
+++ src/Security/DashboardAuthenticator.php
@@ -9,6 +9,7 @@ use App\Enum\TwoFAType;
 use App\Enum\UserTwoFactorAuthenticationStatus;
 use App\Repository\SettingRepository;
 use App\Repository\UserRepository;
+use DateTimeInterface;
 use PixelOpen\CloudflareTurnstileBundle\Http\CloudflareTurnstileHttpClient;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -47,7 +48,7 @@ class DashboardAuthenticator extends AbstractLoginFormAuthenticator
 
         if ($uuid) {
             $user = $this->userRepository->findOneByUUIDAdmin($uuid);
-            if (!$user) {
+            if (!$user instanceof User) {
                 // Validate if the user account exists
                 throw new CustomUserMessageAuthenticationException('Invalid Credentials.');
             }
@@ -55,7 +56,7 @@ class DashboardAuthenticator extends AbstractLoginFormAuthenticator
                 // Validate if the user account exists
                 throw new CustomUserMessageAuthenticationException('This account is currently disabled.');
             }
-            if ($user->getBannedAt() !== null) {
+            if ($user->getBannedAt() instanceof DateTimeInterface) {
                 // Validate if the user account exists
                 throw new CustomUserMessageAuthenticationException('This account is currently banned.');
             }
