84a7fa5a45b6098689715d9bb1cfd33420983ff9
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2025-05-21 09:23:34 +0000

    Update LanguageController to use cookies

--- src/Controller/LanguageController.php
+++ src/Controller/LanguageController.php
@@ -4,21 +4,35 @@ namespace App\Controller;
 
 use App\Enum\LanguagesType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 class LanguageController extends AbstractController
 {
     #[Route('/change-language', name: 'change_language')]
-    public function changeLanguage(Request $request, SessionInterface $session): RedirectResponse
+    public function changeLanguage(Request $request): RedirectResponse
     {
         $locale = $request->query->get('locale', LanguagesType::EN->value);
 
-        $session->set('_locale', $locale);
+        // Only allow supported languages
+        if (!in_array($locale, [LanguagesType::EN->value, LanguagesType::PT->value], true)) {
+            $locale = LanguagesType::EN->value;
+        }
 
-        $referer = $request->headers->get('referer', '/');
-        return $this->redirect($referer);
+        $response = new RedirectResponse($request->headers->get('referer', '/'));
+        $response->headers->setCookie(
+            new Cookie(
+                name: '_locale',
+                value: $locale,
+                expire: time() + (365 * 24 * 60 * 60), // 1 year
+                path: '/',
+                secure: false,
+                httpOnly: false
+            )
+        );
+
+        return $response;
     }
 }
