949e4114cf9d76b1fc0ea55084f934ea0ab55854
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2025-06-13 11:20:07 +0000

    Update twig remove translations user account widget

--- src/Controller/UserAccountDeletionController.php
+++ src/Controller/UserAccountDeletionController.php
@@ -23,7 +23,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 class UserAccountDeletionController extends AbstractController
 {
