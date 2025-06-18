190f857f1ebd67fd09cf68d7fc097f889bb2d9a6
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2025-06-18 16:47:20 +0000

    First commit saving changes returning old code from translations-dev
    this changes are for cron implementation with symfony framework automation

---
+++ src/Schedule.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App;
+
+use Symfony\Component\Scheduler\Attribute\AsSchedule;
+use Symfony\Component\Scheduler\Schedule as SymfonySchedule;
+use Symfony\Component\Scheduler\ScheduleProviderInterface;
+use Symfony\Component\Scheduler\RecurringMessage;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Component\Console\Messenger\RunCommandMessage;
+
+#[AsSchedule]
+readonly class Schedule implements ScheduleProviderInterface
+{
+    public function __construct(
+        private CacheInterface $cache,
+    ) {}
+
+    public function getSchedule(): SymfonySchedule
+    {
+        return new SymfonySchedule()
+            ->stateful($this->cache)
+            ->processOnlyLastMissedRun(true)
+
+            // daily at 00:00
+            ->add(
+                RecurringMessage::cron(
+                    '0 0 * * *',
+                    new RunCommandMessage('clear:deleteUnconfirmedUsers')
+                )
+            )
+
+            // daily at 01:00
+            ->add(
+                RecurringMessage::cron(
+                    '0 1 * * *',
+                    new RunCommandMessage('notify:usersWhenProfileExpires')
+                )
+            );
+    }
+}
+
