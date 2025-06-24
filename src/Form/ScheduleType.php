5960a3de25f0badcbea7bd0ebaf5f557396a313c
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2025-06-24 10:21:17 +0000

    Clean form type ScheduleType.php

--- src/Form/ScheduleType.php
+++ src/Form/ScheduleType.php
@@ -2,20 +2,15 @@
 
 namespace App\Form;
 
-use App\Enum\OperationMode;
-use App\Service\GetSettings;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints as Assert;
 
 class ScheduleType extends AbstractType
 {
-    public function __construct(
-        private readonly GetSettings $getSettings
-    ) {
+    public function __construct()
+    {
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
@@ -24,17 +19,14 @@ class ScheduleType extends AbstractType
             ->add('DELETE_UNCONFIRMED_USERS_CRON', TextType::class, [
                 'label' => 'Delete Unconfirmed Users Cron',
                 'required' => true,
-                'attr' => ['autocomplete' => 'off'],
             ])
             ->add('USERS_WHEN_PROFILE_EXPIRES_CRON', TextType::class, [
                 'label' => 'Notify Users When Profile Expires Cron',
                 'required' => true,
-                'attr' => ['autocomplete' => 'off'],
             ])
             ->add('LDAP_SYNC_CRON', TextType::class, [
                 'label' => 'LDAP Sync Cron',
                 'required' => true,
-                'attr' => ['autocomplete' => 'off'],
             ]);
     }
 
