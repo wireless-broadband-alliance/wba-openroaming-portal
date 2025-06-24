efc5f04aea2f818710a4b008e4f6994a64462824
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2025-06-24 12:11:42 +0000

    Saving time progress about timestamp to be datatime instead of string

--- src/Controller/ScheduleAutomationController.php
+++ src/Controller/ScheduleAutomationController.php
@@ -2,17 +2,15 @@
 
 namespace App\Controller;
 
-use App\Entity\Setting;
 use App\Entity\User;
 use App\Enum\AnalyticalEventType;
-use App\Form\CapportType;
 use App\Form\ScheduleType;
 use App\Repository\SettingRepository;
 use App\Repository\UserRepository;
-use App\Schedule;
 use App\Service\EventActions;
 use App\Service\GetSettings;
 use DateTime;
+use DateTimeInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -22,26 +20,79 @@ use Symfony\Component\Routing\Attribute\Route;
 class ScheduleAutomationController extends AbstractController
 {
 
-    public function __construct(private readonly GetSettings $getSettings,
+    public function __construct(
+        private readonly GetSettings $getSettings,
         private readonly UserRepository $userRepository,
         private readonly SettingRepository $settingRepository,
         private readonly EntityManagerInterface $entityManager,
         private readonly EventActions $eventActions
-    )
-    {
+    ) {
     }
 
     #[Route('/dashboard/settings/schedule', name: 'admin_dashboard_settings_schedule')]
     public function settingsSchedule(Request $request): Response
     {
         $data = $this->getSettings->getSettings($this->userRepository, $this->settingRepository);
-        // Get the current logged-in user (admin)
         /** @var User $currentUser */
         $currentUser = $this->getUser();
 
-        $settings = $this->settingRepository->findAll();
+        $initialData = [];
 
-        $form = $this->createForm(ScheduleType::class, null, [
+        // Loop through the settings you want to edit
+        foreach (
+            [
+                'DELETE_UNCONFIRMED_USERS_CRON',
+                'USERS_WHEN_PROFILE_EXPIRES_CRON',
+                'LDAP_SYNC_CRON'
+            ] as $settingName
+        ) {
+            // Find the setting value from the DB
+            $setting = $this->settingRepository->findOneBy(['name' => $settingName]);
+            $cronValue = $setting ? $setting->getValue() : '';
+
+            // Determine if the cronValue is an "advanced" expression or a simple "frequency + time"
+            // Matches a known pattern we parse it, else fallback to advanced
+            if (preg_match('/^\d+ \d+ \* \* \*$/', $cronValue)) {
+                // This is a daily schedule example "minute hour * * *"
+                // Parse minute and hour from the cronValue
+                [$minute, $hour] = explode(' ', $cronValue);
+
+                $initialData["{$settingName}_frequency"] = 'daily';
+                $initialData["{$settingName}_time"] = DateTime::createFromFormat(
+                    'H:i',
+                    sprintf('%02d:%02d', $hour, $minute)
+                );
+                $initialData["{$settingName}_advanced"] = null;
+            } elseif (preg_match('/^\d+ \d+ \* \* 0$/', $cronValue)) {
+                // weekly
+                [$minute, $hour] = explode(' ', $cronValue);
+
+                $initialData["{$settingName}_frequency"] = 'weekly';
+                $initialData["{$settingName}_time"] = DateTime::createFromFormat(
+                    'H:i',
+                    sprintf('%02d:%02d', $hour, $minute)
+                );
+                $initialData["{$settingName}_advanced"] = null;
+            } elseif (preg_match('/^\d+ \d+ 1 \* \*$/', $cronValue)) {
+                // monthly
+                [$minute, $hour] = explode(' ', $cronValue);
+
+                $initialData["{$settingName}_frequency"] = 'monthly';
+                $initialData["{$settingName}_time"] = DateTime::createFromFormat(
+                    'H:i',
+                    sprintf('%02d:%02d', $hour, $minute)
+                );
+                $initialData["{$settingName}_advanced"] = null;
+            } else {
+                // fallback: treat as advanced expression
+                $initialData["{$settingName}_advanced"] = $cronValue;
+                $initialData["{$settingName}_frequency"] = null;
+                $initialData["{$settingName}_time"] = null;
+            }
+        }
+
+        $settings = $this->settingRepository->findAll();
+        $form = $this->createForm(ScheduleType::class, $initialData, [
             'settings' => $settings,
         ]);
 
@@ -64,10 +115,11 @@ class ScheduleAutomationController extends AbstractController
                     $cronValue = $form->get($settingName . '_advanced')->getData() ?: '';
                 } else {
                     $frequency = $form->get($settingName . '_frequency')->getData();
-                    $time = $form->get($settingName . '_time')->getData(); // string "HH:MM" or null
+                    $time = $form->get($settingName . '_time')->getData();
 
-                    if ($frequency && $time && strpos($time, ':') !== false) {
-                        [$hour, $minute] = explode(':', $time);
+                    if ($frequency && $time instanceof DateTimeInterface) {
+                        $hour = $time->format('H');
+                        $minute = $time->format('i');
 
                         $cronValue = match ($frequency) {
                             'daily' => "$minute $hour * * *",
