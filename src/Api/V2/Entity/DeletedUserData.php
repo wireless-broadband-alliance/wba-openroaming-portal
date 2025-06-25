40a534c41439d37c73797ea171efc8212e6e2b76
rodrigoresendesmoura <rodrigoresendesmoura@tetrapi.pt>
2025-06-25 11:33:19 +0000

    remove unnecessary code and add api prefix on docs

--- src/Api/V2/Entity/DeletedUserData.php
+++ src/Api/V2/Entity/DeletedUserData.php
@@ -373,44 +373,4 @@ use Doctrine\ORM\Mapping as ORM;
 )]
 class DeletedUserData
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
-
-    #[ORM\Column(type: Types::TEXT)]
-    private ?string $pgpEncryptedJsonFile = null;
-
-    #[ORM\OneToOne(inversedBy: 'deletedUserData', cascade: ['persist', 'remove'])]
-    #[ORM\JoinColumn(nullable: false)]
-    private ?User $user = null;
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getPgpEncryptedJsonFile(): ?string
-    {
-        return $this->pgpEncryptedJsonFile;
-    }
-
-    public function setPgpEncryptedJsonFile(string $pgpEncryptedJsonFile): static
-    {
-        $this->pgpEncryptedJsonFile = $pgpEncryptedJsonFile;
-
-        return $this;
-    }
-
-    public function getUser(): ?User
-    {
-        return $this->user;
-    }
-
-    public function setUser(User $user): static
-    {
-        $this->user = $user;
-
-        return $this;
-    }
 }
--- src/Api/V2/Entity/Setting.php
+++ src/Api/V2/Entity/Setting.php
@@ -747,43 +747,4 @@ use Doctrine\ORM\Mapping as ORM;
 )]
 class Setting
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $name = null;
-
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $value = null;
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    public function setName(string $name): self
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    public function getValue(): ?string
-    {
-        return $this->value;
-    }
-
-    public function setValue(?string $value): self
-    {
-        $this->value = $value;
-
-        return $this;
-    }
 }
