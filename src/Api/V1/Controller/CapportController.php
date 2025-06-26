ce7f086e224c4bddc11749cdd7bdfc99ab9c3167
rodrigoresendesmoura <rodrigoresendesmoura@tetrapi.pt>
2025-06-26 16:40:13 +0000

    api controllers update

--- src/Api/V1/Controller/CapportController.php
+++ src/Api/V1/Controller/CapportController.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Api\V1\Controller;
+namespace App\Api\V2\Controller;
 
-use App\Api\V1\BaseResponse;
+use App\Api\V2\BaseResponse;
 use App\Repository\SettingRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -11,34 +11,17 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class CapportController extends AbstractController
 {
-    public function __construct(
-        private readonly SettingRepository $settingRepository
-    ) {
-    }
-
-    #[Route('/capport/json', name: 'api_capport_json', methods: ['GET'])]
-    public function capportJson(): JsonResponse
+    #[Route('/capport/json', name: 'api_v2_capport_json', methods: ['GET'])]
+    public function capportJson(SettingRepository $settingRepository): JsonResponse
     {
-        if (
-            $this->settingRepository->findOneBy(
-                ['name' => 'CAPPORT_ENABLED']
-            )->getValue() !== 'true'
-        ) {
-            return new BaseResponse(
-                Response::HTTP_BAD_REQUEST,
-                null,
-                'CAPPORT is not enabled'
-            )->toResponse();
+        if ($settingRepository->findOneBy(['name' => 'CAPPORT_ENABLED'])->getValue() !== 'true') {
+            return new BaseResponse(Response::HTTP_BAD_REQUEST, null, 'CAPPORT is not enabled')->toResponse();
         }
         return new JsonResponse(
             [
                 'captive' => false,
-                'user-portal-url' => $this->settingRepository->findOneBy(
-                    ['name' => 'CAPPORT_PORTAL_URL']
-                )->getValue(),
-                'venue-info-url' => $this->settingRepository->findOneBy(
-                    ['name' => 'CAPPORT_VENUE_INFO_URL']
-                )->getValue()
+                'user-portal-url' => $settingRepository->findOneBy(['name' => 'CAPPORT_PORTAL_URL'])->getValue(),
+                'venue-info-url' => $settingRepository->findOneBy(['name' => 'CAPPORT_VENUE_INFO_URL'])->getValue()
             ],
             Response::HTTP_OK
         );
