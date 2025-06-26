3699e3cec9f09415d0b1a970f0fc6b7c809f1b56
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2025-06-26 09:56:56 +0000

    start twig tempaltes and try to fetch each route name

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -5,22 +5,49 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\RouterInterface;
 
 class ApiController extends AbstractController
 {
-    public function __construct()
+    public function __construct(private readonly RouterInterface $router)
     {
     }
 
     #[Route('/api/v1', name: 'api_v1_docs')]
     public function versionOne(): Response
     {
-        return $this->render('api/version_one.html.twig');
+        $routes = $this->getRoutesByPrefix('/api/v1');
+
+        return $this->render('api/version_one.html.twig', [
+            'routes' => $routes,
+        ]);
     }
 
     #[Route('/api/v2', name: 'api_v2_docs')]
     public function versionTwo(): Response
     {
-        return $this->render('api/version_two.html.twig');
+        $routes = $this->getRoutesByPrefix('/api/v2');
+
+        return $this->render('api/version_two.html.twig', [
+            'routes' => $routes,
+        ]);
+    }
+
+    private function getRoutesByPrefix(string $prefix): array
+    {
+        $routes = $this->router->getRouteCollection();
+        $filtered = [];
+
+        foreach ($routes as $name => $route) {
+            if (str_starts_with($route->getPath(), $prefix)) {
+                $filtered[] = [
+                    'name' => $name,
+                    'path' => $route->getPath(),
+                    'methods' => $route->getMethods(),
+                ];
+            }
+        }
+
+        return $filtered;
     }
 }
