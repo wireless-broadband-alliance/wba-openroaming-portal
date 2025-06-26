c06b3de6226b9ea277ed4c5f2b48fa99a86fbfe2
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2025-06-26 10:01:20 +0000

    Update ApiController one more time to make a new service
    To handle api endpoints data

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -9,14 +9,14 @@ use Symfony\Component\Routing\RouterInterface;
 
 class ApiController extends AbstractController
 {
-    public function __construct(private readonly RouterInterface $router)
+    public function __construct()
     {
     }
 
     #[Route('/api/v1', name: 'api_v1_docs')]
     public function versionOne(): Response
     {
-        $routes = $this->getRoutesByPrefix('/api/v1');
+        $routes = $this->apiResponseServices->getRoutesByPrefix('/api/v1');
 
         return $this->render('api/version_one.html.twig', [
             'routes' => $routes,
@@ -26,28 +26,12 @@ class ApiController extends AbstractController
     #[Route('/api/v2', name: 'api_v2_docs')]
     public function versionTwo(): Response
     {
-        $routes = $this->getRoutesByPrefix('/api/v2');
+        $routes = $this->apiResponseServices->getRoutesByPrefix('/api/v2');
 
         return $this->render('api/version_two.html.twig', [
             'routes' => $routes,
         ]);
     }
 
-    private function getRoutesByPrefix(string $prefix): array
-    {
-        $routes = $this->router->getRouteCollection();
-        $filtered = [];
 
-        foreach ($routes as $name => $route) {
-            if (str_starts_with($route->getPath(), $prefix)) {
-                $filtered[] = [
-                    'name' => $name,
-                    'path' => $route->getPath(),
-                    'methods' => $route->getMethods(),
-                ];
-            }
-        }
-
-        return $filtered;
-    }
 }
