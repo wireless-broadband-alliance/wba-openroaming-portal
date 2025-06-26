9a723278cbc2f6ebda9b56ff4e0810955c7ff072
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2025-06-26 09:51:48 +0000

    Update api controller add 2 routes

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -3,13 +3,24 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
 class ApiController extends AbstractController
 {
-    public function __construct(){}
-
-    #[Route('/api', name: 'api')]
-    public function api()
+    public function __construct()
     {
+    }
+
+    #[Route('/api/v1', name: 'api_v1_docs')]
+    public function versionOne(): Response
+    {
+        return $this->render('api/version_one.html.twig');
+    }
+
+    #[Route('/api/v2', name: 'api_v2_docs')]
+    public function versionTwo(): Response
+    {
+        return $this->render('api/version_two.html.twig');
     }
 }
