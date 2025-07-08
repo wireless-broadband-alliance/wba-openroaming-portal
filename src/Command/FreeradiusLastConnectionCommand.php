d3713a1c77799bc777186c3e13eca40a57c4a9f8
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2025-07-08 11:11:14 +0000

    Add comment msg for work tracking & details process

--- src/Command/FreeradiusLastConnectionCommand.php
+++ src/Command/FreeradiusLastConnectionCommand.php
@@ -2,18 +2,12 @@
 
 namespace App\Command;
 
-use App\Entity\Setting;
-use App\Entity\User;
-use App\Entity\UserExternalAuth;
-use App\Entity\UserRadiusProfile;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-
-use function Symfony\Component\String\u;
 
 #[AsCommand(
     name: 'backup:freeradiusLastConnection',
@@ -29,6 +23,17 @@ class FreeradiusLastConnectionCommand extends Command
 
     public function backupFreeradiusLastConnection(): int
     {
+        // TODO FOR THIS COMMAND
+        /*
+         * 1 - Check if the connection to the freeradius table exist with the .env DATABASE_FREERADIUS
+         * 2 - Make the query1 to check the radAccount table content -> make the query on the Repo
+         * 3 - Check if the query1 content is the same of the previous execution -> php memory
+         * 4 - Make the logic to update the profile row on the OpenRoaming db UserRadiusProfile "lastConnection" (start/end Connections)
+         * 4.1 - Make the query2 on the OpenRoaming db to get update the rows of each profile if need it
+         * 5 - Output a response message like: "Execution ignored same data checked" if the content on the step 2 is the same
+         * 5.1 - Output a response message like: "Freeradius Connection Times Updated" if the content on the step 2 is diferent
+        */
+
         dd('Freeradius last connection');
     }
 
@@ -36,7 +41,6 @@ class FreeradiusLastConnectionCommand extends Command
     {
         try {
             $this->backupFreeradiusLastConnection();
-
         } catch (Exception $e) {
             // Handle any exceptions and roll back in case of an error
             $this->entityManager->rollback();
