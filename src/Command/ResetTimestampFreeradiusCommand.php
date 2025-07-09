dc382d1e40253cfa4f4ce05113a1a738ed2b2be1
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2025-07-09 12:46:45 +0000

    Add missing import exception

--- src/Command/ResetTimestampFreeradiusCommand.php
+++ src/Command/ResetTimestampFreeradiusCommand.php
@@ -6,6 +6,7 @@ namespace App\Command;
 
 use App\Entity\Setting;
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\QuestionHelper;
