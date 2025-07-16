2f6795c8408ab3f05eec87eae2d23095ce64de0c
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2025-07-16 15:39:46 +0000

    trying to use LiveCollectionTrait to trigger the form error in live time

--- src/Twig/Components/UserUpdateForm.php
+++ src/Twig/Components/UserUpdateForm.php
@@ -3,66 +3,54 @@
 namespace App\Twig\Components;
 
 use App\DTO\UserUpdateDTO;
+use App\Enum\OperationMode;
 use App\Form\UserUpdateType;
+use App\Repository\SettingRepository;
+use App\Service\CronExpressionHelperService;
+use App\Service\SchedulerService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormInterface;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\Attribute\LiveAction;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\ComponentWithFormTrait;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
+use Symfony\UX\LiveComponent\LiveCollectionTrait;
 
 #[AsLiveComponent]
-class UserUpdateForm extends AbstractController
+final class UserUpdateForm extends AbstractController
 {
     use ComponentWithFormTrait;
     use DefaultActionTrait;
+    use LiveCollectionTrait;
 
-    #[LiveProp(writable: true)]
-    public ?UserUpdateDTO $userDTO = null;
-
-    // Holds the Symfony FormInterface instance with errors & data
-    protected FormInterface $form;
-
-    public function __construct(
-        private readonly ValidatorInterface $validator
-    ) {
-    }
+    #[LiveProp]
+    public UserUpdateDTO|null $userUpdateDTO = null;
 
     /**
-     * Initialize or instantiate the form bound to the DTO.
+     * @return FormInterface<mixed>
      */
+    #[\Override]
     protected function instantiateForm(): FormInterface
     {
-        if (!$this->userDTO) {
-            $this->userDTO = new UserUpdateDTO();
-        }
-
-        return $this->createForm(UserUpdateType::class, $this->userDTO);
-    }
-
-    public function mount(?UserUpdateDTO $userDTO = null): void
-    {
-        $this->userDTO = $userDTO ?? new UserUpdateDTO();
-        $this->form = $this->instantiateForm();
+        return $this->createForm(UserUpdateType::class, $this->userUpdateDTO);
     }
 
     #[LiveAction]
     public function validate(): void
     {
         // Rebuild the form with current DTO data
-        $form = $this->createForm(UserUpdateType::class, $this->userDTO);
+        $form = $this->createForm(UserUpdateType::class, $this->userUpdateDTO);
 
         // Submit the form data (simulate form submission) to trigger validation
         $form->submit([
-            'uuid' => $this->userDTO->uuid,
-            'email' => $this->userDTO->email,
-            'firstName' => $this->userDTO->firstName,
-            'lastName' => $this->userDTO->lastName,
-            'phoneNumber' => $this->userDTO->phoneNumber,
-            'isVerified' => $this->userDTO->isVerified,
-            'banned' => $this->userDTO->banned,
+            'uuid' => $this->userUpdateDTO->uuid,
+            'email' => $this->userUpdateDTO->email,
+            'firstName' => $this->userUpdateDTO->firstName,
+            'lastName' => $this->userUpdateDTO->lastName,
+            'phoneNumber' => $this->userUpdateDTO->phoneNumber,
+            'isVerified' => $this->userUpdateDTO->isVerified,
+            'banned' => $this->userUpdateDTO->banned,
         ], false);
 
         // Update form property with new form containing validation results
