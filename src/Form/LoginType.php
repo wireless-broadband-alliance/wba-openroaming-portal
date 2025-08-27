41f4701124dd6f031df150982f8192707e687546
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2025-08-27 16:58:37 +0000

    Remove unecessary condition -> this is already on the DTO

--- src/Form/LoginType.php
+++ src/Form/LoginType.php
@@ -12,13 +12,11 @@ use libphonenumber\PhoneNumberFormat;
 use Misd\PhoneNumberBundle\Form\Type\PhoneNumberType;
 use PixelOpen\CloudflareTurnstileBundle\Type\TurnstileType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\NotBlank;
 
 class LoginType extends AbstractType
 {
@@ -79,11 +77,6 @@ class LoginType extends AbstractType
                 'attr' => [
                     'placeholder' => 'Enter your password',
                     'data-live-ignore' => 'true',
-                ],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Password cannot be empty.',
-                    ]),
                 ],
             ]);
         }
