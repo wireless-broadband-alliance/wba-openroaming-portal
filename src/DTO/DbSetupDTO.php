dc292b26c2fd7badc5c55b8340b59ef841b1c672
rodrigoresendesmoura <rodrigoresendesmoura@tetrapi.pt>
2025-10-06 12:29:56 +0000

    first form with validations

--- src/DTO/DbSetupDTO.php
+++ src/DTO/DbSetupDTO.php
@@ -6,10 +6,22 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 class DbSetupDTO
 {
-    #[Assert\NotBlank(message: 'UUIDNotBlank')]
+    #[Assert\NotBlank(message: 'fieldNotBlank')]
+    #[Assert\Length(
+        min: 15,
+        max: 255,
+        minMessage: 'minCharacters',
+        maxMessage: 'maxCharacters'
+    )]
     public ?string $dbOpenRoaming = null;
 
-    #[Assert\NotBlank(message: 'UUIDNotBlank')]
+    #[Assert\NotBlank(message: 'fieldNotBlank')]
+    #[Assert\Length(
+        min: 15,
+        max: 255,
+        minMessage: 'minCharacters',
+        maxMessage: 'maxCharacters'
+    )]
     public ?string $dbFreeradius = null;
 
 }
