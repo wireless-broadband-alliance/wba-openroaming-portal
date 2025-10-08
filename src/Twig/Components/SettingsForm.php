1722d1f6b933639ccca6086d96abe6a2352decae
rodrigoresendesmoura <rodrigoresendesmoura@tetrapi.pt>
2025-10-08 11:34:02 +0000

    settings validations

--- src/Twig/Components/SettingsForm.php
+++ src/Twig/Components/SettingsForm.php
@@ -2,10 +2,14 @@
 
 namespace App\Twig\Components;
 
+use App\DTO\DbSetupDTO;
 use App\DTO\SettingsDTO;
+use App\Form\DbSetupType;
+use App\Form\SettingsType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormInterface;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveAction;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\ComponentWithFormTrait;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
@@ -20,7 +24,7 @@ class SettingsForm extends AbstractController
     use LiveCollectionTrait;
 
     #[LiveProp]
-    public SettingsDTO|null $dbSetupDTO = null;
+    public SettingsDTO|null $settingsDTO = null;
 
     /**
      * @return FormInterface<mixed>
@@ -28,7 +32,30 @@ class SettingsForm extends AbstractController
     #[\Override]
     protected function instantiateForm(): FormInterface
     {
-        return $this->createForm(DbSetupType::class, $this->dbSetupDTO);
+        return $this->createForm(SettingsType::class, $this->settingsDTO);
+    }
+
+    #[LiveAction]
+    public function validate(): void
+    {
+        if (!$this->settingsDTO instanceof SettingsDTO) {
+            $this->settingsDTO = new SettingsDTO();
+        }
+
+        // Rebuild form with DTO data
+        $form = $this->createForm(SettingsType::class, $this->settingsDTO);
+
+        // Submit the form data to trigger validation
+        $form->submit([
+            'trustedProxies' => $this->settingsDTO->trustedProxies,
+            'turnstileKey' => $this->settingsDTO->turnstileKey,
+            'turnstileSecret' => $this->settingsDTO->turnstileSecret,
+            'jwtSecretKey' => $this->settingsDTO->jwtSecretKey,
+            'jwtPublicKey' => $this->settingsDTO->jwtPublicKey,
+            'jwtPassphrase' => $this->settingsDTO->jwtPassphrase,
+        ], false);
+
+        $this->form = $form;
     }
 
 }
