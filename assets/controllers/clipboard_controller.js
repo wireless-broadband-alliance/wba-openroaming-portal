348c966beccae4b3ac29068914eae39e30c5748c
rodrigoresendesmoura <rodrigoresendesmoura@tetrapi.pt>
2025-10-14 16:39:34 +0000

    fix "copy to clipboard"

--- assets/controllers/clipboard_controller.js
+++ assets/controllers/clipboard_controller.js
@@ -3,19 +3,16 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
     static targets = ["code"];
 
-    copyToClipboard(event) {
-        let pre;
+    copyToClipboard() {
+        const textToCopy = this.codeTarget.innerText;
 
-        if (event) {
-            pre = event.currentTarget.closest('.space-y-2').querySelector('pre[data-clipboard-target]');
-        }
-
-        if (!pre) {
-            pre = this.codeTarget;
-        }
-
-        navigator.clipboard.writeText(pre.innerText)
-            .then(() => alert("Text copied to clipboard!"))
-            .catch(() => alert("Unable to copy the text."));
+        navigator.clipboard
+            .writeText(textToCopy)
+            .then(() => {
+                alert("Text copied to clipboard!");
+            })
+            .catch(() => {
+                alert("Unable to copy the text.");
+            });
     }
 }
