bc7e0a5fbb785477835da6ea83156ebdf2a447bc
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2025-10-17 12:04:41 +0000

    Add warning msg on the dashboard incase a process is aborted
    Still need to block the download options on the landing page for today

---
+++ src/Twig/CertificateProcessExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace App\Twig;
+
+use App\Service\CertificateProcessCheckerService;
+use App\Enum\CertificateProcessStatus;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class CertificateProcessExtension extends AbstractExtension
+{
+    public function __construct(
+        private readonly CertificateProcessCheckerService $certificateProcessCheckerService
+    ) {
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('isCertificateAborted', [$this, 'isCertificateAborted']),
+        ];
+    }
+
+    /**
+     * Check if the current certificate process is aborted
+     */
+    public function isCertificateAborted(): bool
+    {
+        $currentProcess = $this->certificateProcessCheckerService->getCurrentProcess();
+
+        if ($currentProcess === null) {
+            return false;
+        }
+
+        return $currentProcess->getStatus() === CertificateProcessStatus::ABORTED;
+    }
+}
