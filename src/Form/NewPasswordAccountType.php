729586ff8501b419e05ff62dc08500e8c451295d
rodrigoresendesmoura <rodrigoresendesmoura@tetrapi.pt>
2025-10-27 12:09:09 -0100

    fix pipelines (phpStan) -> NewPasswordAccountType.php

--- src/Form/NewPasswordAccountType.php
+++ src/Form/NewPasswordAccountType.php
@@ -13,6 +13,9 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
+/**
+ * @extends AbstractType<User>
+ */
 class NewPasswordAccountType extends AbstractType
 {
     public function __construct(
