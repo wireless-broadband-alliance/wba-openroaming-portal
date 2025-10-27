fa027bbaac0ef61ccc5a6c43f983c194d84053d1
rodrigoresendesmoura <rodrigoresendesmoura@tetrapi.pt>
2025-10-27 12:18:19 -0100

    fix pipelines (phpStan) -> AccountUserUpdateLandingType.php

--- src/Form/AccountUserUpdateLandingType.php
+++ src/Form/AccountUserUpdateLandingType.php
@@ -10,6 +10,9 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+/**
+ * @extends AbstractType<User>
+ */
 class AccountUserUpdateLandingType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
