bafafc3322e8451ba0c13ad29c63a340458d6a80
rodrigoresendesmoura <rodrigoresendesmoura@tetrapi.pt>
2025-10-27 12:13:24 -0100

    fix pipelines (phpStan) -> ForgotPasswordEmailType.php

--- src/Form/ForgotPasswordEmailType.php
+++ src/Form/ForgotPasswordEmailType.php
@@ -13,6 +13,10 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
+
+/**
+ * @extends AbstractType<null>
+ */
 class ForgotPasswordEmailType extends AbstractType
 {
     public function __construct(
