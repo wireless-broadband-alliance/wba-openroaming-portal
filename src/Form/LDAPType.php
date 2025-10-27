1db34f20319384b5cae43507eb481e8c1fc20c5b
rodrigoresendesmoura <rodrigoresendesmoura@tetrapi.pt>
2025-10-27 12:11:54 -0100

    fix pipelines (phpStan) -> LDAPType.php

--- src/Form/LDAPType.php
+++ src/Form/LDAPType.php
@@ -13,6 +13,9 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
+/**
+ * @extends AbstractType<null>
+ */
 class LDAPType extends AbstractType
 {
     public function __construct(
