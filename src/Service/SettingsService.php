86ceed13eb755941c498e942ae7b27223a77099e
rodrigoresendesmoura <rodrigoresendesmoura@tetrapi.pt>
2025-10-31 12:02:13 -0100

    save settings with translations

--- src/Service/SettingsService.php
+++ src/Service/SettingsService.php
@@ -3,14 +3,18 @@
 namespace App\Service;
 
 use App\Entity\Setting;
+use App\Enum\LanguageType;
+use App\Enum\SettingName;
 use App\Repository\SettingRepository;
+use App\Repository\SettingTranslationRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
 readonly class SettingsService
 {
     public function __construct(
         private SettingRepository $settingRepository,
-        private EntityManagerInterface $entityManager
+        private EntityManagerInterface $entityManager,
+        private SettingTranslationRepository $settingTranslationRepository,
     ) {
     }
 
@@ -39,6 +43,58 @@ readonly class SettingsService
 
             if ($setting) {
                 $setting->setValue($value);
+            } else {
+                // Create new setting if it doesn't exist
+                $setting = new Setting();
+                $setting->setName($name);
+                $setting->setValue($value);
+                $this->entityManager->persist($setting);
+            }
+        }
+    }
+
+    /**
+     * Update or create multiple settings from a generic array.
+     *
+     * @param array<string, array{value: string|null, description?: string}> $settingsData
+     */
+    public function updateAuthSettingsToTranslateFromArray(array $settingsData, ?string $locale = LanguageType::EN->value): void
+    {
+        $authSettingsToTranslate = [
+            SettingName::AUTH_METHOD_SAML_LABEL->value,
+            SettingName::AUTH_METHOD_GOOGLE_LOGIN_LABEL->value,
+            SettingName::AUTH_METHOD_MICROSOFT_LOGIN_LABEL->value,
+            SettingName::AUTH_METHOD_REGISTER_LABEL->value,
+            SettingName::AUTH_METHOD_LOGIN_TRADITIONAL_LABEL->value,
+            SettingName::AUTH_METHOD_SMS_REGISTER_LABEL->value,
+            SettingName::AUTH_METHOD_SAML_DESCRIPTION->value,
+            SettingName::AUTH_METHOD_GOOGLE_LOGIN_DESCRIPTION->value,
+            SettingName::AUTH_METHOD_MICROSOFT_LOGIN_DESCRIPTION->value,
+            SettingName::AUTH_METHOD_REGISTER_DESCRIPTION->value,
+            SettingName::AUTH_METHOD_LOGIN_TRADITIONAL_DESCRIPTION->value,
+            SettingName::AUTH_METHOD_SMS_REGISTER_DESCRIPTION->value,
+        ];
+        foreach ($settingsData as $name => $item) {
+            $value = $item['value'] ?? null;
+
+            // Try to fetch existing setting
+            $setting = $this->settingRepository->findOneBy(['name' => $name]);
+
+            if ($setting) {
+                if (in_array($name, $authSettingsToTranslate, true)) {
+                    // Get the translated setting
+                    $settingTranslation = $this->settingTranslationRepository->findOneBy(
+                        ['setting' => $setting, 'locale' => $locale]
+                    );
+                    if ($value === null) {
+                        $settingTranslation?->setTranslation('');
+                    } else {
+                        $settingTranslation?->setTranslation($value);
+                    }
+                } else {
+                    $setting->setValue($value);
+                }
+
             } else {
                 // Create new setting if it doesn't exist
                 $setting = new Setting();
