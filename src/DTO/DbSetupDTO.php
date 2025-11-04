9e4d20a3fc4c539c443932a8c7e5d62d76ebb507
rodrigoresendesmoura <rodrigoresendesmoura@tetrapi.pt>
2025-11-04 16:14:47 -0100

    rework db config form and dto to ave more fields instead of url only

--- src/DTO/DbSetupDTO.php
+++ src/DTO/DbSetupDTO.php
@@ -13,7 +13,7 @@ class DbSetupDTO
         minMessage: 'minCharacters',
         maxMessage: 'maxCharacters'
     )]
-    public ?string $dbOpenRoaming = null;
+    public ?string $dbOpenRoamingUserName = null;
 
     #[Assert\NotBlank(message: 'fieldNotBlank')]
     #[Assert\Length(
@@ -22,5 +22,49 @@ class DbSetupDTO
         minMessage: 'minCharacters',
         maxMessage: 'maxCharacters'
     )]
-    public ?string $dbFreeradius = null;
+    public ?string $dbOpenRoamingPassword = null;
+
+    #[Assert\NotBlank(message: 'fieldNotBlank')]
+    #[Assert\Length(
+        min: 15,
+        max: 255,
+        minMessage: 'minCharacters',
+        maxMessage: 'maxCharacters'
+    )]
+    public ?string $dbOpenRoamingIp = null;
+
+    #[Assert\NotBlank(message: 'fieldNotBlank')]
+    #[Assert\GreaterThanOrEqual(value: 1, message: 'portCannotBeLessThan')]
+    public ?int $dbOpenRoamingPort = null;
+
+    #[Assert\NotBlank(message: 'fieldNotBlank')]
+    #[Assert\Length(
+        min: 15,
+        max: 255,
+        minMessage: 'minCharacters',
+        maxMessage: 'maxCharacters'
+    )]
+    public ?string $dbFreeradiusUserName = null;
+
+    #[Assert\NotBlank(message: 'fieldNotBlank')]
+    #[Assert\Length(
+        min: 15,
+        max: 255,
+        minMessage: 'minCharacters',
+        maxMessage: 'maxCharacters'
+    )]
+    public ?string $dbFreeradiusPassword = null;
+
+    #[Assert\NotBlank(message: 'fieldNotBlank')]
+    #[Assert\Length(
+        min: 15,
+        max: 255,
+        minMessage: 'minCharacters',
+        maxMessage: 'maxCharacters'
+    )]
+    public ?string $dbFreeradiusIp = null;
+
+    #[Assert\NotBlank(message: 'fieldNotBlank')]
+    #[Assert\GreaterThanOrEqual(value: 1, message: 'portCannotBeLessThan')]
+    public ?int $dbFreeradiusPort = null;
 }
--- src/Form/DbSetupType.php
+++ src/Form/DbSetupType.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Form;
 
 use App\DTO\DbSetupDTO;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,10 +16,28 @@ class DbSetupType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('dbOpenRoaming', TextType::class, [
+            ->add('dbOpenRoamingUserName', TextType::class, [
                 'required' => false,
             ])
-            ->add('dbFreeradius', TextType::class, [
+            ->add('dbOpenRoamingPassword', TextType::class, [
+                'required' => false,
+            ])
+            ->add('dbOpenRoamingIp', TextType::class, [
+                'required' => false,
+            ])
+            ->add('dbOpenRoamingPort', IntegerType::class, [
+                'required' => false,
+            ])
+            ->add('dbFreeradiusUserName', TextType::class, [
+                'required' => false,
+            ])
+            ->add('dbFreeradiusPassword', TextType::class, [
+                'required' => false,
+            ])
+            ->add('dbFreeradiusIp', TextType::class, [
+                'required' => false,
+            ])
+            ->add('dbFreeradiusPort', IntegerType::class, [
                 'required' => false,
             ]);
     }
