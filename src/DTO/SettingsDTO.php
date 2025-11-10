1810fd5f805f53c5e11c4428f036dfc8d73563eb
rodrigoresendesmoura <rodrigoresendesmoura@tetrapi.pt>
2025-11-10 15:09:01 -0100

    add new validation for trusted proxies, (ip validations)

--- src/DTO/SettingsDTO.php
+++ src/DTO/SettingsDTO.php
@@ -3,6 +3,7 @@
 namespace App\DTO;
 
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 class SettingsDTO
 {
@@ -18,4 +19,19 @@ class SettingsDTO
     public ?bool $jwtPassphraseEnable = false;
 
     public ?string $jwtPassphrase = null;
+
+
+    #[Assert\Callback]
+    public function validateTrustedProxies(ExecutionContextInterface $context)
+    {
+        $ips = array_map('trim', explode(',', $this->trustedProxies));
+        foreach ($ips as $ip) {
+            if (!filter_var($ip, FILTER_VALIDATE_IP)) {
+                $context->buildViolation('notValidIp')
+                    ->atPath('trustedProxies')
+                    ->setParameter('%value%', $ip)
+                    ->addViolation();
+            }
+        }
+    }
 }
