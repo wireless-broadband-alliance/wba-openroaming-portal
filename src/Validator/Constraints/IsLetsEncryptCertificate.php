899b832f4bf484234d8c5f33d4ef432f5e71b3b6
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2025-11-20 16:09:11 -0100

    Add validator msg on the validator
    I know this validator design need and actual translation bcs its only called to display a flash warning on the page

--- src/Validator/Constraints/IsLetsEncryptCertificate.php
+++ src/Validator/Constraints/IsLetsEncryptCertificate.php
@@ -7,7 +7,7 @@ use Symfony\Component\Validator\Constraint;
 #[\Attribute]
 class IsLetsEncryptCertificate extends Constraint
 {
-    public string $message = 'The uploaded certificate is not issued by Let\'s Encrypt.';
+    public string $message = 'certificateIsLetsEncrypt';
 
     public function validatedBy(): string
     {
