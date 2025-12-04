b107c5f478b0122e0e19ad967072a42685900dbf
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2025-12-04 15:36:45 -0100

    WIP saving progress this is still dead and untested

--- src/Api/V2/Controller/TwoFAController.php
+++ src/Api/V2/Controller/TwoFAController.php
@@ -5,6 +5,7 @@ namespace App\Api\V2\Controller;
 use App\Api\V2\BaseResponse;
 use App\Entity\User;
 use App\Enum\AnalyticalEventType;
+use App\Enum\UserProvider;
 use App\Enum\UserTwoFactorAuthenticationStatus;
 use App\Service\EventActions;
 use App\Service\JWTTokenGenerator;
@@ -23,197 +24,178 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
 class TwoFAController extends AbstractController
 {
-    public function __construct(
-        private readonly UserStatusChecker $userStatusChecker,
-        private readonly TwoFAService $twoFAService,
-        private readonly EventActions $eventActions,
-        private readonly TokenStorageInterface $tokenStorage,
-        private readonly JWTTokenGenerator $JWTTokenGenerator,
-        private readonly TOTPService $TOTPService,
-        private readonly EntityManagerInterface $entityManager,
-    ) {
-    }
+  public function __construct(
+      private readonly UserStatusChecker $userStatusChecker,
+      private readonly TwoFAService $twoFAService,
+      private readonly EventActions $eventActions,
+      private readonly TokenStorageInterface $tokenStorage,
+      private readonly JWTTokenGenerator $JWTTokenGenerator,
+      private readonly TOTPService $TOTPService,
+      private readonly EntityManagerInterface $entityManager,
+  ) {
+  }
 
-    #[Route(
-        '/twoFA/{type}',
-        name: 'api_v2_twoFA_enable',
-        requirements: [
-            'type' => 'totp|email|sms',
-        ],
-        defaults: [
-            'type' => 'email',
-        ],
-        methods: ['POST'])]
-    public function twoFAEnable(Request $request, string $type): JsonResponse
-    {
-        $token = $this->tokenStorage->getToken();
+  #[Route(
+      '/twoFA/{type}',
+      name: 'api_v2_twoFA_enable',
+      requirements: [
+          'type' => 'totp|email|sms',
+      ],
+      defaults: [
+          'type' => 'email',
+      ],
+      methods: ['POST'])]
+  public function twoFAEnable(Request $request, string $type): JsonResponse
+  {
+    $token = $this->tokenStorage->getToken();
 
-        if ($token instanceof TokenInterface && $token->getUser() instanceof User) {
-            /** @var User $currentUser */
-            $currentUser = $token->getUser();
-            // This line is begin ignore because the getCredentials belongs to another service
-            /** @phpstan-ignore-next-line */
-            $jwtTokenString = $token->getCredentials();
+    if ($token instanceof TokenInterface && $token->getUser() instanceof User) {
+      /** @var User $currentUser */
+      $currentUser = $token->getUser();
+      // This line is begin ignore because the getCredentials belongs to another service
+      /** @phpstan-ignore-next-line */
+      $jwtTokenString = $token->getCredentials();
 
-            if (!$this->JWTTokenGenerator->isJWTTokenValid($jwtTokenString)) {
-                return new BaseResponse(
-                    401,
-                    null,
-                    'JWT Token is invalid!'
-                )->toResponse();
-            }
-
-            $statusCheckerResponse = $this->userStatusChecker->checkUserStatus($currentUser);
-            if ($statusCheckerResponse instanceof BaseResponse) {
-                return $statusCheckerResponse->toResponse();
-            }
-
-            if ($type === 'totp') {
-                $secret = $this->TOTPService->generateSecret();
-                $currentUser->setTwoFAsecret($secret);
-                $this->entityManager->persist($currentUser);
-                $this->entityManager->flush();
-                // Utilize the toApiResponse method to generate the response content
-                $content = $currentUser->toApiResponse([
-                    'success' => true,
-                    'msg' => 'Two Factor TOTP Secret generated successfully',
-                    'totpId' => $currentUser->getTwoFAsecret(),
-                ]);
-            } else {
-                $this->twoFAService->generate2FACode(
-                    $currentUser,
-                    $request->getClientIp(),
-                    $request->headers->get('User-Agent'),
-                    AnalyticalEventType::TWO_FA_CODE_ENABLE->value
-                );
-
-                // Utilize the toApiResponse method to generate the response content
-                $content = $currentUser->toApiResponse([
-                    'success' => true,
-                    'msg' => 'Code sent successfully.',
-                    'totpId' => null,
-                ]);
-            }
-
-            return new BaseResponse(200, $content)->toResponse();
-        }
-
-        // Handle the case where the user is not authenticated
+      if (!$this->JWTTokenGenerator->isJWTTokenValid($jwtTokenString)) {
         return new BaseResponse(
-            403,
+            401,
             null,
-            'Unauthorized - You do not have permission to access this resource'
-        )->toResponse(); // Bad Request Response
+            'JWT Token is invalid!'
+        )->toResponse();
+      }
 
-    }
+      $statusCheckerResponse = $this->userStatusChecker->checkUserStatus($currentUser);
+      if ($statusCheckerResponse instanceof BaseResponse) {
+        return $statusCheckerResponse->toResponse();
+      }
 
-    #[Route('/twoFA/validate', name: 'api_v2_twoFA_validate', methods: ['POST'])]
-    public function twoFAValidate(Request $request): JsonResponse
-    {
-        try {
-            $data = json_decode($request->getContent(), true, 512, JSON_THROW_ON_ERROR);
-        } catch (JsonException) {
-            return new BaseResponse(400, null, 'Invalid JSON format')->toResponse(); # Bad Request Response
-        }
-
-        $token = $this->tokenStorage->getToken();
-
-        if ($token instanceof TokenInterface && $token->getUser() instanceof User) {
-            /** @var User $currentUser */
-            $currentUser = $token->getUser();
-            // This line is begin ignore because the getCredentials belongs to another service
-            /** @phpstan-ignore-next-line */
-            $jwtTokenString = $token->getCredentials();
-
-            if (!$this->JWTTokenGenerator->isJWTTokenValid($jwtTokenString)) {
-                return new BaseResponse(
-                    401,
-                    null,
-                    'JWT Token is invalid!'
-                )->toResponse();
-            }
-
-            $statusCheckerResponse = $this->userStatusChecker->checkUserStatus($currentUser);
-            if ($statusCheckerResponse instanceof BaseResponse) {
-                return $statusCheckerResponse->toResponse();
-            }
-
-            if (
-                ($data['type'] === 'email' || $data['type'] === 'sms') &&
-                $data['code'] === $currentUser->getTwoFAcode()
-            ) {
-                if ($data['type'] === 'email') {
-                    $currentUser->setTwoFAtype(UserTwoFactorAuthenticationStatus::EMAIL->value);
-                }
-                if ($data['type'] === 'sms') {
-                    $currentUser->setTwoFAtype(UserTwoFactorAuthenticationStatus::SMS->value);
-                }
-                $this->eventAndSaveUser(
-                    $request,
-                    $currentUser,
-                    AnalyticalEventType::ENABLE_LOCAL_2FA->value
-                );
-
-                $content = $currentUser->toApiResponse([
-                    'success' => true,
-                    'msg' => 'Two Factor authentication validated successfully!',
-                ]);
-
-                return new BaseResponse(200, $content)->toResponse();
-            }
-
-            if ($data['type'] === 'totp' && $this->TOTPService->verifyTOTP(
-                    $currentUser->getTwoFAsecret(),
-                    $data['code']
-                )) {
-                $currentUser->setTwoFAtype(UserTwoFactorAuthenticationStatus::TOTP->value);
-                $this->eventAndSaveUser(
-                    $request,
-                    $currentUser,
-                    AnalyticalEventType::ENABLE_TOTP_2FA->value
-                );
-                $content = $currentUser->toApiResponse([
-                    'success' => true,
-                    'msg' => 'Two Factor authentication validated successfully!',
-                ]);
-
-                return new BaseResponse(200, $content)->toResponse();
-            }
-
-            return new BaseResponse(
-                403,
-                null,
-                'Invalid code'
-            )->toResponse();
-        }
-
-        // Handle the case where the user is not authenticated
-        return new BaseResponse(
-            403,
-            null,
-            'Unauthorized - You do not have permission to access this resource'
-        )->toResponse(); // Bad Request Response
-
-    }
-
-    private function eventAndSaveUser(Request $request, User $currentUser, string $event): void
-    {
+      // Only manually update the 2fa totp if the user checks it
+      if ($type === 'totp') {
+        $secret = $this->TOTPService->generateSecret();
+        $currentUser->setTwoFAsecret($secret);
         $this->entityManager->persist($currentUser);
         $this->entityManager->flush();
 
-        // Defines the Event to the table
-        $eventMetadata = [
-            'ip' => $request->getClientIp(),
-            'user_agent' => $request->headers->get('User-Agent'),
-            'uuid' => $currentUser->getUuid(),
+        $content = [
+            'totpId' => $currentUser->getTwoFAsecret()
         ];
 
-        $this->eventActions->saveEvent(
-            $currentUser,
-            $event,
-            new DateTime(),
-            $eventMetadata
-        );
+        return new BaseResponse(
+            200,
+            $content,
+            'Two Factor TOTP Secret generated successfully'
+        )->toResponse();
+      }
+
+      // Auto-detects if the user did select email||sms
+      $this->twoFAService->generate2FACode(
+          $currentUser,
+          $request->getClientIp(),
+          $request->headers->get('User-Agent'),
+          AnalyticalEventType::TWO_FA_CODE_ENABLE->value
+      );
+
+      return new BaseResponse(
+          200,
+          null,
+          'Two Factor Code send to you successfully'
+      )->toResponse();
     }
 
+    // Handle the case where the user is not authenticated
+    return new BaseResponse(
+        403,
+        null,
+        'Unauthorized - You do not have permission to access this resource'
+    )->toResponse(); // Bad Request Response
+  }
+
+  #[Route('/twoFA/validate', name: 'api_v2_twoFA_validate', methods: ['POST'])]
+  public function twoFAValidate(Request $request): JsonResponse
+  {
+    try {
+      $data = json_decode($request->getContent(), true, 512, JSON_THROW_ON_ERROR);
+    } catch (JsonException) {
+      return new BaseResponse(400, null, 'Invalid JSON format')->toResponse(); # Bad Request Response
+    }
+
+    $token = $this->tokenStorage->getToken();
+
+    if ($token instanceof TokenInterface && $token->getUser() instanceof User) {
+      /** @var User $currentUser */
+      $currentUser = $token->getUser();
+      // This line is begin ignore because the getCredentials belongs to another service
+      /** @phpstan-ignore-next-line */
+      $jwtTokenString = $token->getCredentials();
+
+      if (!$this->JWTTokenGenerator->isJWTTokenValid($jwtTokenString)) {
+        return new BaseResponse(
+            401,
+            null,
+            'JWT Token is invalid!'
+        )->toResponse();
+      }
+
+      $statusCheckerResponse = $this->userStatusChecker->checkUserStatus($currentUser);
+      if ($statusCheckerResponse instanceof BaseResponse) {
+        return $statusCheckerResponse->toResponse();
+      }
+
+      if (
+          ($data['type'] === 'email' || $data['type'] === 'sms') &&
+          $data['code'] === $currentUser->getTwoFAcode()
+      ) {
+        if ($data['type'] === 'email') {
+          $currentUser->setTwoFAtype(UserTwoFactorAuthenticationStatus::EMAIL->value);
+        }
+        if ($data['type'] === 'sms') {
+          $currentUser->setTwoFAtype(UserTwoFactorAuthenticationStatus::SMS->value);
+        }
+        $this->eventAndSaveUser(
+            $request,
+            $currentUser,
+            AnalyticalEventType::ENABLE_LOCAL_2FA->value
+        );
+
+        $content = $currentUser->toApiResponse([
+            'success' => true,
+            'msg' => 'Two Factor authentication validated successfully!',
+        ]);
+
+        return new BaseResponse(200, $content)->toResponse();
+      }
+
+      if ($data['type'] === 'totp' && $this->TOTPService->verifyTOTP(
+              $currentUser->getTwoFAsecret(),
+              $data['code']
+          )) {
+        $currentUser->setTwoFAtype(UserTwoFactorAuthenticationStatus::TOTP->value);
+        $this->eventAndSaveUser(
+            $request,
+            $currentUser,
+            AnalyticalEventType::ENABLE_TOTP_2FA->value
+        );
+        $content = $currentUser->toApiResponse([
+            'success' => true,
+            'msg' => 'Two Factor authentication validated successfully!',
+        ]);
+
+        return new BaseResponse(200, $content)->toResponse();
+      }
+
+      return new BaseResponse(
+          403,
+          null,
+          'Invalid code'
+      )->toResponse();
+    }
+
+    // Handle the case where the user is not authenticated
+    return new BaseResponse(
+        403,
+        null,
+        'Unauthorized - You do not have permission to access this resource'
+    )->toResponse(); // Bad Request Response
+
+  }
 }
