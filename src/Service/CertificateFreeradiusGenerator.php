a3e5404742cf4c94a88c63e33c331e47fe30dc61
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2025-12-10 11:42:02 -0100

    Update docker file again add new packages and apply changes with
    ca-certificates
    Also add new gitkeeps for each folder for certbot command and service in use -> CertificateFreeradiusGenerator

--- src/Service/CertificateFreeradiusGenerator.php
+++ src/Service/CertificateFreeradiusGenerator.php
@@ -2,26 +2,28 @@
 
 namespace App\Service;
 
+use App\Entity\User;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class CertificateFreeradiusGenerator
 {
-  public function generateCertificate(string $domain): void
+  public function generateCertificates(string $domain, User $user): void
   {
     // Build certbot command
     $command = [
         'certbot',
         'certonly',
-        '-d',
-        $domain,
-        '--key-type',
-        'rsa',
-        '--rsa-key-size',
-        '2048',
+        '-d', $domain,
+        '--key-type', 'rsa',
+        '--rsa-key-size', '2048',
         '--non-interactive',
         '--agree-tos',
-        '--standalone'
+        '--email', $user->getEmail(),
+        '--standalone',
+        '--config-dir', '/var/www/openroaming/var/certs/config',
+        '--work-dir', '/var/www/openroaming/var/certs/work',
+        '--logs-dir', '/var/www/openroaming/var/certs/logs'
     ];
 
     $process = new Process($command);
