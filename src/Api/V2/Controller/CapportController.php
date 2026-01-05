bcc04e42863f5a41d9e749085d8ab1500a2507eb
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2026-01-05 19:12:22 -0100

    fix psr12 capport

--- src/Api/V2/Controller/CapportController.php
+++ src/Api/V2/Controller/CapportController.php
@@ -15,21 +15,32 @@ class CapportController extends AbstractController
     public function __construct(private readonly SettingRepository $settingRepository)
     {
     }
+
     #[Route('/capport/json', name: 'api_v2_capport_json', methods: ['GET'])]
     public function capportJson(): JsonResponse
     {
-        if ($this->settingRepository->findOneBy(['name' => SettingName::CAPPORT_ENABLED->value])->getValue() !== 'true') {
-            return new BaseResponse(Response::HTTP_BAD_REQUEST, null, 'CAPPORT is not enabled')->toResponse();
+        if (
+            $this->settingRepository->findOneBy(
+                [
+                'name' => SettingName::CAPPORT_ENABLED->value
+                ]
+            )->getValue() !== 'true'
+        ) {
+            return new BaseResponse(
+                Response::HTTP_BAD_REQUEST,
+                null,
+                'CAPPORT is not enabled'
+            )->toResponse();
         }
         return new JsonResponse(
             [
-                'captive' => false,
-                'user-portal-url' => $this->settingRepository->findOneBy([
-                    'name' => SettingName::CAPPORT_PORTAL_URL->value
-                ])->getValue(),
-                'venue-info-url' => $this->settingRepository->findOneBy([
-                    'name' => SettingName::CAPPORT_VENUE_INFO_URL->value
-                ])->getValue()
+            'captive' => false,
+            'user-portal-url' => $this->settingRepository->findOneBy([
+                'name' => SettingName::CAPPORT_PORTAL_URL->value
+            ])->getValue(),
+            'venue-info-url' => $this->settingRepository->findOneBy([
+                'name' => SettingName::CAPPORT_VENUE_INFO_URL->value
+            ])->getValue()
             ],
             Response::HTTP_OK
         );
