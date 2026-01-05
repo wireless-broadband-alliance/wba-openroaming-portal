a72585f540b3f41545c5dc1c82f889790836a4eb
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2026-01-05 16:34:55 -0100

    fix psr12

--- src/Api/V1/Controller/CapportController.php
+++ src/Api/V1/Controller/CapportController.php
@@ -18,9 +18,11 @@ class CapportController extends AbstractController
     #[Route('/capport/json', name: 'api_v1_capport_json', methods: ['GET'])]
     public function capportJson(): JsonResponse
     {
-        if ($this->settingRepository->findOneBy(
-            ['name' => SettingName::CAPPORT_ENABLED->value]
-            )->getValue() !== 'true') {
+        if (
+            $this->settingRepository->findOneBy(
+                ['name' => SettingName::CAPPORT_ENABLED->value]
+            )->getValue() !== 'true'
+        ) {
             return new BaseResponse(
                 Response::HTTP_BAD_REQUEST,
                 null,
