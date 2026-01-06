eb0ee0ef001d5dac95446dea3e806d84181110c9
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2026-01-06 14:39:57 -0100

    Fix minor details with the merge

--- assets/controllers/nav-dropdown_controller.js
+++ assets/controllers/nav-dropdown_controller.js
@@ -1,27 +1,29 @@
-import { Controller } from '@hotwired/stimulus';
+import {Controller} from '@hotwired/stimulus';
 
 export default class extends Controller {
-  static targets = ['button', 'container'];
-  static values = { toggleSelected: { type: Boolean, default: true } };
+    static targets = ['button', 'container'];
+    static values = {toggleSelected: {type: Boolean, default: true}};
 
-  toggle() {
-    this.containerTarget.classList.toggle('hidden');
-
-    if (this.toggleSelectedValue) {
-      this.buttonTarget.classList.toggle('selected');
-    }
-  }
-
-  lost_focus() {
-    if (!this.containerTarget.matches(':hover')) {
-      this.containerTarget.classList.add('hidden');
-
-      if (this.toggleSelectedValue) {
-        this.buttonTarget.classList.remove('selected');
-      }
-    }
-
-    toggleHeader() {
+    toggle() {
         this.containerTarget.classList.toggle('hidden');
+
+        if (this.toggleSelectedValue) {
+            this.buttonTarget.classList.toggle('selected');
+        }
+    }
+
+    lost_focus() {
+        if (!this.containerTarget.matches(':hover')) {
+            this.containerTarget.classList.add('hidden');
+
+            if (this.toggleSelectedValue) {
+                this.buttonTarget.classList.remove('selected');
+            }
+        }
+
+        toggleHeader()
+        {
+            this.containerTarget.classList.toggle('hidden');
+        }
     }
 }
