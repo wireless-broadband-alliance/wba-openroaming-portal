556553e9a2dbece3c21e8dc3f0d56577584b6e29
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2026-01-06 12:27:53 -0100

    Fix prettier pipeline

--- assets/controllers/terms-widget_controller.js
+++ assets/controllers/terms-widget_controller.js
@@ -1,65 +1,65 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-  static targets = ['agreeTerms', 'confirmationModal', 'button'];
+    static targets = ['agreeTerms', 'confirmationModal', 'button'];
 
-  connect() {
-    fetch('/get-terms-status')
-      .then((res) => res.json())
-      .then((data) => {
-        this.updateTermsCheckbox(data.termsAccepted);
+    connect() {
+        fetch('/get-terms-status')
+            .then((res) => res.json())
+            .then((data) => {
+                this.updateTermsCheckbox(data.termsAccepted);
+                this.toggleSubmitButtons();
+            });
+    }
+
+    showModal(event) {
+        event.preventDefault();
+        // Show the modal if the terms checkbox is not checked
+        if (!this.agreeTermsTarget.checked) {
+            this.confirmationModalTarget.classList.remove('hidden');
+        } else {
+            // Get href of clicked link
+            window.location.href = event.currentTarget.getAttribute('href');
+        }
+    }
+
+    handleCheckboxChange() {
+        const isChecked = this.agreeTermsTarget.checked;
+
+        fetch(isChecked ? '/accept-terms' : '/reject-terms', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data.message);
+            });
+
+        // Show/Hide warning widget based on the checkbox state if the user clicks any of the authentication methods
+        if (isChecked) this.closeConfirmationModal();
+
         this.toggleSubmitButtons();
-      });
-  }
-
-  showModal(event) {
-    event.preventDefault();
-    // Show the modal if the terms checkbox is not checked
-    if (!this.agreeTermsTarget.checked) {
-      this.confirmationModalTarget.classList.remove('hidden');
-    } else {
-      // Get href of clicked link
-      window.location.href = event.currentTarget.getAttribute('href');
     }
-  }
 
-  handleCheckboxChange() {
-    const isChecked = this.agreeTermsTarget.checked;
-
-    fetch(isChecked ? '/accept-terms' : '/reject-terms', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data.message);
-      });
-
-    // Show/Hide warning widget based on the checkbox state if the user clicks any of the authentication methods
-    if (isChecked) this.closeConfirmationModal();
-
-    this.toggleSubmitButtons();
-  }
-
-  updateTermsCheckbox(accepted) {
-    if (this.agreeTermsTarget) {
-      this.agreeTermsTarget.checked = accepted;
+    updateTermsCheckbox(accepted) {
+        if (this.agreeTermsTarget) {
+            this.agreeTermsTarget.checked = accepted;
+        }
     }
-  }
 
-  toggleSubmitButtons() {
-    const isChecked = this.agreeTermsTarget?.checked || false;
-    for (let button of this.buttonTargets) {
-      // btn-disabled (for general buttons)
-      button.classList.toggle('btn-disabled', !isChecked);
-      if (button.classList.contains('btn-secondary')) {
-        // btn-secondary-disabled (for the specific login button)
-        button.classList.toggle('btn-secondary-disabled', !isChecked);
-      }
+    toggleSubmitButtons() {
+        const isChecked = this.agreeTermsTarget?.checked || false;
+        for (let button of this.buttonTargets) {
+            // btn-disabled (for general buttons)
+            button.classList.toggle('btn-disabled', !isChecked);
+            if (button.classList.contains('btn-secondary')) {
+                // btn-secondary-disabled (for the specific login button)
+                button.classList.toggle('btn-secondary-disabled', !isChecked);
+            }
+        }
     }
-  }
 
-  closeConfirmationModal() {
-    this.confirmationModalTarget.classList.add('hidden');
-  }
+    closeConfirmationModal() {
+        this.confirmationModalTarget.classList.add('hidden');
+    }
 }
--- assets/controllers/toast_controller.js
+++ assets/controllers/toast_controller.js
@@ -1,20 +1,20 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-  static targets = ['toast'];
+    static targets = ['toast'];
 
-  connect() {
-    super.connect();
+    connect() {
+        super.connect();
 
-    if (this.hasToastTarget) {
-      console.log(
-        '%c Visibility - Detected for ' + this.toastTarget.name,
-        'background: green; color: black'
-      );
+        if (this.hasToastTarget) {
+            console.log(
+                '%c Visibility - Detected for ' + this.toastTarget.name,
+                'background: green; color: black'
+            );
+        }
     }
-  }
 
-  close() {
-    this.toastTarget.classList.add('hidden');
-  }
+    close() {
+        this.toastTarget.classList.add('hidden');
+    }
 }
