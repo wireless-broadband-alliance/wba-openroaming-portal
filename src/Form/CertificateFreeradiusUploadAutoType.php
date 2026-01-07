cac3b292346e1cce9fa4e36e735efd8e036ada23
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2026-01-07 11:45:46 -0100

    fix phpstan CertificateFreeradiusUploadAutoType

--- src/Form/CertificateFreeradiusUploadAutoType.php
+++ src/Form/CertificateFreeradiusUploadAutoType.php
@@ -9,6 +9,9 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Vich\UploaderBundle\Form\Type\VichFileType;
 
+/**
+ * @extends AbstractType<CertificateFreeradiusUploadAutoDTO>
+ */
 class CertificateFreeradiusUploadAutoType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
