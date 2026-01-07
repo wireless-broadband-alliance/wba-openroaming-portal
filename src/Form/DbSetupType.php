905e6ae2db5bc920f775264de5caf5ac9e5b1ca5
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2026-01-07 11:43:17 -0100

    Fix phpstan DbSetupType

--- src/Form/DbSetupType.php
+++ src/Form/DbSetupType.php
@@ -14,6 +14,9 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
+/**
+ * @extends AbstractType<DbSetupDTO>
+ */
 class DbSetupType extends AbstractType
 {
     public function __construct(
