e5b52e41225759cba68189686c8ac77bf55c799d
rodrigoresendesmoura <rodrigoresendesmoura@tetrapi.pt>
2026-01-14 14:47:16 -0100

    fix pipelines (phpstan)

--- src/Form/SourceBlacklistType.php
+++ src/Form/SourceBlacklistType.php
@@ -16,7 +16,7 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 
 class SourceBlacklistType extends AbstractType
 {
-    public function __construct(private readonly TranslatorInterface $translator)
+    public function __construct()
     {
     }
 
