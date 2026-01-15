3d10c411d01ac954a60e90c2070cf9bdfd54a68b
rodrigoresendesmoura <rodrigoresendesmoura@tetrapi.pt>
2026-01-15 15:53:40 -0100

    fix migration with default value for permission

--- migrations/Version20251210163905.php
+++ migrations/Version20251210163905.php
@@ -20,7 +20,7 @@ final class Version20251210163905 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE User ADD permissions JSON NOT NULL');
+        $this->addSql("ALTER TABLE User ADD permissions JSON NOT NULL DEFAULT '[]'");
     }
 
     public function down(Schema $schema): void
