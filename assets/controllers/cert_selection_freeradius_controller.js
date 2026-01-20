6fa81dff89f8a4ea05657aa7c0db476d23ae3f71
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2026-01-20 09:48:24 -0100

    Rework freeradius selection certs generation page

---
+++ assets/controllers/cert_selection_freeradius_controller.js
@@ -0,0 +1,20 @@
+import {Controller} from '@hotwired/stimulus';
+
+export default class extends Controller {
+    static targets = [
+        'initialOptions',
+        'letsEncryptOptions'
+    ];
+
+    selectLetsEncrypt(event) {
+        event.preventDefault();
+
+        this.initialOptionsTarget.classList.add('hidden');
+        this.letsEncryptOptionsTarget.classList.remove('hidden');
+    }
+
+    back() {
+        this.letsEncryptOptionsTarget.classList.add('hidden');
+        this.initialOptionsTarget.classList.remove('hidden');
+    }
+}
