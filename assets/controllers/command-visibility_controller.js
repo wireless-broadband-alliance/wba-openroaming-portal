9abcba325373d8ad286cd0bef24f49912cd667ef
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2026-02-11 15:00:42 -0100

    Fix (Show/Hide) Label not updating
    And also remove the bluer from the first code block

--- assets/controllers/command-visibility_controller.js
+++ assets/controllers/command-visibility_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-    static targets = ['hiddenBlock', 'visibleBlock'];
+    static targets = ['hiddenBlock', 'visibleBlock', 'toggleButton'];
     static values = { opened: { type: Boolean, default: false } };
 
     connect() {
@@ -17,9 +17,11 @@ export default class extends Controller {
         if (this.openedValue) {
             this.visibleBlockTarget.classList.remove('hidden');
             this.hiddenBlockTarget.classList.add('hidden');
+            this.toggleButtonTarget.textContent = this.toggleButtonTarget.dataset.hideText;
         } else {
             this.visibleBlockTarget.classList.add('hidden');
             this.hiddenBlockTarget.classList.remove('hidden');
+            this.toggleButtonTarget.textContent = this.toggleButtonTarget.dataset.showText;
         }
     }
 }
