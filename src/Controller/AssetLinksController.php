a3db7f59ecda5ce39df4129de5641fdb81a5af14
Marcelo Fernandes <marcelofernandes@tetrapi.pt>
2026-05-04 11:13:40 +0000

    Attach proper route name instead of hard coded text

--- src/Controller/AssetLinksController.php
+++ src/Controller/AssetLinksController.php
@@ -38,6 +38,7 @@ class AssetLinksController extends AbstractController
         private readonly SettingsService $settingsService,
         private readonly EntityManagerInterface $entityManager,
         private readonly ReturnAppFingerprintRepository $returnAppFingerprintRepository,
+        private readonly RouterInterface $router,
     ) {
     }
 
@@ -110,7 +111,7 @@ class AssetLinksController extends AbstractController
         $appIds = $appIds ? [$appIds] : [];
 
         // Add the corresponding for the app redirection
-        $path = 'return-to-app';
+        $path = ltrim($this->router->generate('app_return_to_app'), '/');
         $components = [
             [
                 '/' => '/' . $path,
